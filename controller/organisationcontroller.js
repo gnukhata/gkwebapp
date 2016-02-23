c644686452ec8d684f9f0aaaf630ca9fedb48af9
abhi1tb <abhijithb21@openmailbox.org>
2016-02-23 12:20:20 +0530

    Made changes in Main Shell. It now has a fixed sidebar and a toggle button for toolbar. Separated control logic from template.

--- controller/organisationcontroller.js
+++ controller/organisationcontroller.js
@@ -96,3 +96,15 @@ orgs.controller("login", function($scope, $window, $http, orgcode) {
 	 }
 })
 
+var main_shell = angular.module('main_shell', ['ui.directives']);
+main_shell.controller('shellcontroller', function($scope) {
+    $scope.count=0;
+    $scope.flag =0;
+    $scope.show = function() {
+		$scope.flag = $scope.count%2;
+        return $scope.flag;
+    };    
+    $scope.keypresscount = function () {
+    	$scope.count = $scope.count+1;
+  	};
+});
