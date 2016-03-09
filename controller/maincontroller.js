f44b2bfedff0e2887064b3f4a36557dedcd7b77b
Abhijith B <abhijithb21@openmailbox.org>
2016-03-09 18:36:24 +0530

    Created Mainshell using bootstrap and included jquery in the project.

--- controller/maincontroller.js
+++ controller/maincontroller.js
@@ -1,4 +1,4 @@
-var mainapp = angular.module('mainapp', ['ui.directives','ngRoute']);
+var mainapp = angular.module('mainapp', ['ngRoute']);
 mainapp.config([
              '$routeProvider', function($routeProvider) {
                     $routeProvider.when('/account', {
@@ -13,18 +13,3 @@ mainapp.config([
                     });
               }
            ]);
-
-
-
-mainapp.controller('mainController', function($scope, $window) {
-    $scope.count=0;
-    $scope.flag =0;
-    $scope.show = function() {
-		$scope.flag = $scope.count%2;
-        return $scope.flag;
-    };    
-    $scope.keypresscount = function () {
-    	$scope.count = $scope.count+1;
-  	};
-  	
-});
