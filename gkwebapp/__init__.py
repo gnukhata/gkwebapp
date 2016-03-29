061c1b76283772d1ef7b2e330e15c7ff7c2ade39
ishan masdekar <imasdekar@dff.org.in>
2016-03-29 17:43:28 +0530

    Flow complete till mainshell through createorg

--- gkwebapp/__init__.py
+++ gkwebapp/__init__.py
@@ -25,6 +25,7 @@ def main(global_config, **settings):
     config.add_route('createorg', '/createorg')
     config.add_route('yearcode', '/yearcode')
     config.add_route('login', '/login')
+    config.add_route('showmainshell', '/showmainshell')
     config.add_route('createadmin', '/createadmin')
     config.scan('gkwebapp')
     return config.make_wsgi_app()
