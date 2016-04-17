db96db51f220be37ce0125759fecc965f347ab87
ishan masdekar <imasdekar@dff.org.in>
2016-04-17 17:53:02 +0530

    done all the necessary validations and finishing work on find voucher.

--- gkwebapp/static/js/findeditvoucher.js
+++ gkwebapp/static/js/findeditvoucher.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 $("#searchby").focus();
-$('.vdate').autotab('number');
+$('.vcdate').autotab('number');
 $(".vtp").hide();
-$(".vno").hide();
+$(".vn").hide();
 $(".amt").hide();
 $(".vdate").hide();
 $(".nar").hide();
@@ -14,47 +14,72 @@ $("#searchby").bind("change keyup",function(event) {
   if (search=="type")
   {
     $(".vtp").show();
-    $(".vno").hide();
+    $(".vn").hide();
     $(".amt").hide();
     $(".vdate").hide();
     $(".nar").hide();
 
+    if (event.which==13)
+    {
+      $("#vtype").focus();
+    }
+
   }
   else if (search=="vnum")
   {
-    $(".vno").show();
+    $(".vn").show();
     $(".vtp").hide();
     $(".amt").hide();
     $(".vdate").hide();
     $(".nar").hide();
 
+    if (event.which==13)
+    {
+      $("#vnum").focus();
+    }
+
   }
   else if (search=="amount")
   {
-    $(".vno").hide();
+    $(".vn").hide();
     $(".vtp").hide();
     $(".amt").show();
     $(".vdate").hide();
     $(".nar").hide();
 
+    if (event.which==13)
+    {
+      $("#amount").focus();
+    }
+
   }
   else if (search=="date")
   {
-    $(".vno").hide();
+    $(".vn").hide();
     $(".vtp").hide();
     $(".amt").hide();
     $(".vdate").show();
     $(".nar").hide();
 
+    if (event.which==13)
+    {
+      $("#fday").focus();
+    }
+
   }
 
   else if (search=="narration")
   {
-    $(".vno").hide();
+    $(".vn").hide();
     $(".vtp").hide();
     $(".amt").hide();
     $(".vdate").hide();
     $(".nar").show();
+
+    if (event.which==13)
+    {
+      $("#narration").focus();
+    }
 
   }
 
@@ -107,7 +132,124 @@ $(document).on('keyup' ,'.vno',function(event) {
     });
 
 
+$("#reset").click(function()
+{
+  $('#fevoucher').click();
+}
+);
+
+
 $("#findvoucher").submit(function(event) {
+
+
+
+var search = $("#searchby option:selected").val();
+if (search=="type")
+{
+  if ($('#vtype').val()=="") {
+    $("#vtype-alert").alert();
+    $("#vtype-alert").fadeTo(2000, 500).slideUp(500, function(){
+      $("#vtype-alert").alert('close');
+    });
+    $('#vtype').focus();
+    return false;
+  }
+}
+else if (search=="vnum")
+{
+  if ($('#vnum').val()=="") {
+    $("#vno-alert").alert();
+    $("#vno-alert").fadeTo(2000, 500).slideUp(500, function(){
+      $("#vno-alert").alert('close');
+    });
+    $('#vnum').focus();
+    return false;
+  }
+}
+else if (search=="amount")
+{
+  if ($('#amount').val()=="") {
+    $("#vamt-alert").alert();
+    $("#vamt-alert").fadeTo(2000, 500).slideUp(500, function(){
+      $("#vamt-alert").alert('close');
+    });
+    $('#amount').focus();
+    return false;
+  }
+}
+else if (search=="date")
+{
+  if ($('#fday').val()=="") {
+    $("#vdt-alert").alert();
+    $("#vdt-alert").fadeTo(2000, 500).slideUp(500, function(){
+      $("#vdt-alert").alert('close');
+    });
+    $('#fday').focus();
+    return false;
+  }
+
+  if ($('#fmonth').val()=="") {
+    $("#vdt-alert").alert();
+    $("#vdt-alert").fadeTo(2000, 500).slideUp(500, function(){
+      $("#vdt-alert").alert('close');
+    });
+    $('#fmonth').focus();
+    return false;
+  }
+
+  if ($('#fyear').val()=="") {
+    $("#vdt-alert").alert();
+    $("#vdt-alert").fadeTo(2000, 500).slideUp(500, function(){
+      $("#vdt-alert").alert('close');
+    });
+    $('#fyear').focus();
+    return false;
+  }
+
+  if ($('#tday').val()=="") {
+    $("#vdt-alert").alert();
+    $("#vdt-alert").fadeTo(2000, 500).slideUp(500, function(){
+      $("#vdt-alert").alert('close');
+    });
+    $('#tday').focus();
+    return false;
+  }
+
+  if ($('#tmonth').val()=="") {
+    $("#vdt-alert").alert();
+    $("#vdt-alert").fadeTo(2000, 500).slideUp(500, function(){
+      $("#vdt-alert").alert('close');
+    });
+    $('#tmonth').focus();
+    return false;
+  }
+  if ($('#tyear').val()=="") {
+    $("#vdt-alert").alert();
+    $("#vdt-alert").fadeTo(2000, 500).slideUp(500, function(){
+      $("#vdt-alert").alert('close');
+    });
+    $('#tyear').focus();
+    return false;
+  }
+
+}
+
+else if (search=="narration")
+{
+  if ($('#narration').val()=="") {
+    $("#vnar-alert").alert();
+    $("#vnar-alert").fadeTo(2000, 500).slideUp(500, function(){
+      $("#vnar-alert").alert('close');
+    });
+    $('#narration').focus();
+    return false;
+  }
+
+}
+
+
+
+
 $(".vtab").show();
 $(".table").empty();
 $(".table").append('<thead>'+
@@ -139,7 +281,12 @@ $.ajax({
         success: function(jsonObj)
         {
           var vdetails=jsonObj["vouchers"];
-
+          if (vdetails=="")
+          {
+          alert("No Transactions Found with the Specified "+ $("#searchby option:selected").text());
+          }
+          else
+          {
             for(vc in vdetails)
             {
               var DRS = vdetails[vc].drs;
@@ -206,6 +353,8 @@ $.ajax({
               );
 
             };
+          }
+
             var links = $('table a');
             links.eq(links.index(this)-1).focus();
             $('#vtable tr:first-child').addClass('selected');
