3bd737822f48d9b88757a77f186a05af6304b305
abhi1tb <abhijithb21@openmailbox.org>
2016-05-15 11:21:52 +0530

    Now user can navigate using enter/up arrow in createuser page
    Added navigation using enter/up arrow functionality in createuser.js
    Removed disabled select a role option from corresponnding template

--- gkwebapp/static/js/createuser.js
+++ gkwebapp/static/js/createuser.js
@@ -6,7 +6,29 @@ $(document).ready(function(){
     equalTo: "#password"
   }
 }
-
+});
+var inselect = 0;
+$("#userrole").focus(function(){
+  inselect = 1;
+});
+$("input,select").keydown(function(e) {
+  var n = $("input,select").length;
+  var f = $('input,select');
+  var s1 = $("#userrole option:selected").index();
+  if (e.which == 13)
+  {
+    var nextIndex = f.index(this) + 1;
+    if(nextIndex < n){
+      e.preventDefault();
+      f[nextIndex].focus();}
+  }
+  if (e.which == 38 && ((inselect == 1 && s1 == 0) || inselect == 0))
+  {
+    var prevIndex = f.index(this) - 1;
+    if(prevIndex < n){
+      e.preventDefault();
+      f[prevIndex].focus();}
+  }
   });
   $("#adduser").submit(function(e)
   {
