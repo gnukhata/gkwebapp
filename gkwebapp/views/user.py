2915682d235f479317c1d7c55b844d012baf0035
svinayak <vinayakkusumkar@openmailbox.com>
2016-05-23 08:11:20 +0530

    Modification and validation of the create user template.

--- gkwebapp/views/user.py
+++ gkwebapp/views/user.py
@@ -13,14 +13,7 @@ def createuser(request):
     gkdata = {"username":request.params["username"],"userpassword":request.params["userpassword"],"userrole":int(request.params["userrole"]),"userquestion":request.params["userquestion"],"useranswer":request.params["useranswer"]}
     print gkdata
     result = requests.post("http://127.0.0.1:6543/users", data =json.dumps(gkdata), headers=headers)
-    if result.json()["gkstatus"]==0:
-        return {"gkstatus":"User added successfully"}
-    elif result.json()["gkstatus"]==4:
-        return {"gkstatus":"Access denied!"}
-    elif result.json()["gkstatus"]==1:
-        return {"gkstatus":"User already exists!"}
-    elif result.json()["gkstatus"]==3:
-        return {"gkstatus":"Connection Failed"}
+    return {"gkstatus":result.json()["gkstatus"]}
 
 @view_config(route_name="removeuser", renderer="gkwebapp:templates/removeUser.jinja2")
 def removeuser(request):
