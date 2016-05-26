4fc6f0c9fd1de7383badd78f7cbcdf9fcf9d6894
ishan masdekar <imasdekar@dff.org.in>
2016-05-27 00:21:02 +0530

    completed multiple accounts module.

--- gkwebapp/static/js/multiacc.js
+++ gkwebapp/static/js/multiacc.js
@@ -7,31 +7,32 @@ $(document).ready(function() {
     if (event.which==13)
     {
       var curindex = $(this).closest('tr').index();
-      var accname = $('#m_acctable tbody tr:eq('+curindex+') td:eq(0) input').val()
-      if (accname=="")
+      var m_grpnm = $("#m_gname").val();
+      if(m_grpnm=="Select Group" || m_grpnm=="Direct Expense" || m_grpnm=="Direct Income" || m_grpnm=="Indirect Expense" || m_grpnm=="Indirect Income")
       {
-        return false;
+
+          if ($(this).closest('tr').is(":last-child"))
+          {
+            addRow(curindex);
+          }
+          else
+          {
+            $('#m_acctable tbody tr:eq('+nextindex+') td:eq(0) input:enabled').focus();
+            $('#m_acctable tbody tr:eq('+nextindex+') td:eq(0) input:enabled').select();
+          }
       }
       else
       {
-        var m_grpnm = $("#m_gname").val();
-        if(m_grpnm=="Select Group" || m_grpnm=="Direct Expense" || m_grpnm=="Direct Income" || m_grpnm=="Indirect Expense" || m_grpnm=="Indirect Income")
-        {
-          $(".m_add").click();
-        }
-        else
-        {
-          $('#m_acctable tbody tr:eq('+curindex+') td:eq(1) input').focus();
-        }
+        $('#m_acctable tbody tr:eq('+curindex+') td:eq(1) input:enabled').focus();
       }
+
     }
   });
 
-  $(document).off("click",".m_add").on("click", ".m_add", function()
+  function addRow(curindex)
   {
 
-    var curindex = $(this).closest('tr').index();
-    var accname = $('#m_acctable tbody tr:eq('+curindex+') td:eq(0) input').val()
+    var accname = $('#m_acctable tbody tr:eq('+curindex+') td:eq(0) input').val();
     if (accname=="")
     {
 
@@ -42,16 +43,166 @@ $(document).ready(function() {
       $('#m_acctable tbody tr:eq('+curindex+') td:eq(0) input').focus();
       return false;
     }
+
     $.ajax({
       url: '/accountexists',
       type: 'POST',
       datatype: 'json',
-      data: {"accountname": accname}
+      data: {"accountname": accname},
+      beforeSend: function(xhr)
+      {
+        xhr.setRequestHeader('gktoken',sessionStorage.gktoken );
+      }
     })
-    .done(function() {
-      
-    })
+    .done(function(jsonobj)
+    {
+      if (jsonobj["gkstatus"]==1)
+      {
+        $("#m_duplicate-alert").alert();
+        $("#m_duplicate-alert").fadeTo(2000, 500).slideUp(500, function(){
+          $("#m_blank-alert").hide();
+        });
+        $('#m_acctable tbody tr:eq('+curindex+') td:eq(0) input').focus();
+        $('#m_acctable tbody tr:eq('+curindex+') td:eq(0) input').select();
+      }
+      if (jsonobj["gkstatus"]==0)
+      {
+        if ($('#m_acctable tbody tr:eq('+curindex+') td:eq(0) input').is(":disabled"))
+        {
+          $("#m_acctable").append('<tr>'+
+          '<td class="col-xs-7"><input type="text" id="m_alt_accname" class="form-control input-sm m_accname" placeholder="Account Name"></td>'+
+          '<td class="col-xs-3">'+
+          '<input type="text" class=" form-control input-sm m_openbal rightJustified" placeholder="0.00" disabled>'+
+          '</td>'+
+          '<td class="col-xs-2">'+
+          '<a href="#" class="m_del"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a>'+
+          '</td>'+
+          '</tr>'
+        );
+      }
+      else
+      {
+        $("#m_acctable").append('<tr>'+
+        '<td class="col-xs-7"><input type="text" id="m_alt_accname" class="form-control input-sm m_accname" placeholder="Account Name"></td>'+
+        '<td class="col-xs-3">'+
+        '<input type="text" class=" form-control input-sm m_openbal rightJustified" placeholder="0.00">'+
+        '</td>'+
+        '<td class="col-xs-2">'+
+        '<a href="#" class="m_del"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a>'+
+        '</td>'+
+        '</tr>'
+      );
 
+    }
+
+
+    $('#m_acctable tbody tr:last td:eq(0) input').focus();
+  }
+});
+
+}
+
+$(document).off("keyup",".m_openbal").on("keyup",".m_openbal", function(event)
+{
+  if(event.which == 13)
+  {
+    var curindex = $(this).closest('tr').index();
+    var nextindex = curindex+1;
+    if ($('#m_acctable tbody tr:eq('+curindex+') td:eq(1) input:enabled').val()==0 || $('#m_acctable tbody tr:eq('+curindex+') td:eq(1) input:enabled').val()=="")
+    {
+      $('#m_acctable tbody tr:eq('+curindex+') td:eq(1) input:enabled').val("0.00");
+    }
+
+    if ($(this).closest('tr').is(":last-child"))
+    {
+      addRow(curindex);
+    }
+    else
+    {
+      $('#m_acctable tbody tr:eq('+nextindex+') td:eq(0) input:enabled').focus();
+      $('#m_acctable tbody tr:eq('+nextindex+') td:eq(0) input:enabled').select();
+    }
+
+  }
+
+});
+
+$(document).off("click",".m_del").on("click", ".m_del", function() {
+  $(this).closest('tr').fadeOut(200, function(){
+    $(this).closest('tr').remove();   //closest method gives the closest element specified
+    $('#m_acctable tbody tr:last td:eq(0) input').focus();
   });
+  $('#m_acctable tbody tr:last td:eq(0) input').select();
+});
+$(document).off("click",".#acc_add").on("click", "#acc_add", function() {
+
+
+  var output = [];  // This is an array which will contain dictionaries representing rows of the table.
+  $("#m_acctable tbody tr").each(function() { //loop for the rows of the table body
+
+    var obj = {};
+
+    obj.accountname = $(".m_accname", this).val();
+    if ($(".m_openbal", this).val()!="")
+    {
+      obj.openbal = $(".m_openbal", this).val();
+    }
+    else {
+      obj.openbal = "0.00";
+    }
+    obj.groupname = $("#m_gcode").val();
+    obj.subgroupname = $("#m_sgcode").val();
+    obj.newsubgroup = $("#m_nsgcode").val();
+    output.push(obj);
+  });
+
+
+
+
+  $.ajax({
+    url: '/multiacc',
+    type: 'POST',
+    datatype: 'json',
+    data: {"accdetails": JSON.stringify(output)},
+    beforeSend: function(xhr)
+    {
+      xhr.setRequestHeader('gktoken',sessionStorage.gktoken );
+    }
+  })
+  .done(function(resp) {
+
+    if(resp["gkstatus"]==0)
+    {
+      $("#m_multiacc").modal('hide');
+      $("#reset").click();
+      $('.modal-backdrop').remove();
+      $("#success-alert").alert();
+      $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
+        $("#success-alert").hide();
+      });
+
+    }
+    else if(resp["gkstatus"]==1)
+    {
+      $("#m_multiacc").modal('hide');
+      $("#duplicate-alert").alert();
+      $("#duplicate-alert").fadeTo(2000, 500).slideUp(500, function(){
+        $("#duplicate-alert").hide();
+      });
+      $("#accname").focus().select();
+    }
+    else
+    {
+      $("#m_multiacc").modal('hide');
+      $("#failure-alert").alert();
+      $("#failure-alert").fadeTo(2000, 500).slideUp(500, function(){
+        $("#failure-alert").hide();
+      });
+      $("#accname").focus().select();
+    }
+  });
+
+});
+
 
 });
