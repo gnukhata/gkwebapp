63caf5f908d3b93db51cfd14d0557b1fdc6ee546
Vaibhav Kurhe <vaibspidy@openmailbox.org>
2016-06-09 02:55:06 +0530

    spaces to tabs fixed

--- gkwebapp/static/js/trialbalancereport.js
+++ gkwebapp/static/js/trialbalancereport.js
@@ -1,196 +1,196 @@
 $(document).ready(function() {
-  $('.trialbaltable tbody tr:first-child td:eq(1) a').focus();
-  $('.trialbaltable tbody tr:first-child td:eq(1) a').closest('tr').addClass('selected');
-  var percentwid = 100*(($(".table-fixedheader").width()-12)/$(".table-fixedheader").width());
-  $('.table-fixedheader thead').width(percentwid+"%");
-  var percentheigth = 100*(($("body").height()-$(".navbar").height()-148)/$("body").height());
-  $('.table-fixedheader tbody').height(percentheigth+"%");
+	$('.trialbaltable tbody tr:first-child td:eq(1) a').focus();
+	$('.trialbaltable tbody tr:first-child td:eq(1) a').closest('tr').addClass('selected');
+	var percentwid = 100*(($(".table-fixedheader").width()-12)/$(".table-fixedheader").width());
+	$('.table-fixedheader thead').width(percentwid+"%");
+	var percentheigth = 100*(($("body").height()-$(".navbar").height()-148)/$("body").height());
+	$('.table-fixedheader tbody').height(percentheigth+"%");
 
-  $(document).off('focus' ,'.accname').on('focus' ,'.accname',function() {
-    $('.trialbaltable tr').removeClass('selected');
-    $(this).closest('tr').addClass('selected');
-  });
+	$(document).off('focus' ,'.accname').on('focus' ,'.accname',function() {
+		$('.trialbaltable tr').removeClass('selected');
+		$(this).closest('tr').addClass('selected');
+	});
 
-  $(document).off('blur' ,'.accname').on('blur' ,'.accname',function() {
-    $('.trialbaltable tr').removeClass('selected');
+	$(document).off('blur' ,'.accname').on('blur' ,'.accname',function() {
+		$('.trialbaltable tr').removeClass('selected');
 
-  });
-  var curindex ;
-  var nextindex;
-  var previndex;
-  var date = $("#ledtodate").val().split("-");
-  var newtodate = date[2]+"-"+date[1]+"-"+date[0];
+	});
+	var curindex ;
+	var nextindex;
+	var previndex;
+	var date = $("#ledtodate").val().split("-");
+	var newtodate = date[2]+"-"+date[1]+"-"+date[0];
 
-  $(document).off('keydown' ,'.accname').on('keydown' ,'.accname',function(event) {
-    curindex = $(this).closest('tr').index();
-    nextindex = curindex+1;
-    previndex = curindex-1;
-    if (event.which==40)
-    {
-      event.preventDefault();
-      $('.trialbaltable tbody tr:eq('+nextindex+') td:eq(1) a').focus();
-    }
-    else if (event.which==38)
-    {
-      if(previndex>-1)
-      {
-        event.preventDefault();
-        $('.trialbaltable tbody tr:eq('+previndex+') td:eq(1) a').focus();
-      }
-    }
+	$(document).off('keydown' ,'.accname').on('keydown' ,'.accname',function(event) {
+		curindex = $(this).closest('tr').index();
+		nextindex = curindex+1;
+		previndex = curindex-1;
+		if (event.which==40)
+		{
+			event.preventDefault();
+			$('.trialbaltable tbody tr:eq('+nextindex+') td:eq(1) a').focus();
+		}
+		else if (event.which==38)
+		{
+			if(previndex>-1)
+			{
+				event.preventDefault();
+				$('.trialbaltable tbody tr:eq('+previndex+') td:eq(1) a').focus();
+			}
+		}
 
-  });
+	});
 
-  var urole = $("#urole").val();
+	var urole = $("#urole").val();
 
 
-  $(".trialbaltable").off('click','tr').on('click','tr',function(e){
-    e.preventDefault();
-    var id = $(this).attr('value');
-    var currindex = $(this).index();
-    $('.trialbaltable tr').removeClass('selected');
-    $(this).toggleClass('selected');
-    $('.trialbaltable tbody tr:eq('+currindex+') a').focus();
+	$(".trialbaltable").off('click','tr').on('click','tr',function(e){
+		e.preventDefault();
+		var id = $(this).attr('value');
+		var currindex = $(this).index();
+		$('.trialbaltable tr').removeClass('selected');
+		$(this).toggleClass('selected');
+		$('.trialbaltable tbody tr:eq('+currindex+') a').focus();
 
-  });
+	});
 
-  $(".trialbaltable").off('keydown','tr').on('keydown','tr',function(e){
-    var id = $(this).attr('value');
-    var rindex = $(this).index();
+	$(".trialbaltable").off('keydown','tr').on('keydown','tr',function(e){
+		var id = $(this).attr('value');
+		var rindex = $(this).index();
 
-    if(e.which==13)
-    {
+		if(e.which==13)
+		{
 
-    $('.trialbaltable tbody tr:eq('+rindex+')').dblclick() ;
-    }
+		$('.trialbaltable tbody tr:eq('+rindex+')').dblclick() ;
+		}
 });
 
-  $(".trialbaltable tbody tr").off('dblclick').on('dblclick',function(e){
-    e.preventDefault();
-    var acccode = $(this).attr('value');
-    if (acccode=="")
-    {
-        return false;
-    }
-     var date = $("#ledtodate").val().split("-");
-     var newtodate = date[2]+"-"+date[1]+"-"+date[0];
-    $.ajax(
-      {
-        type: "POST",
-        url: "/showledgerreport",
-        global: false,
-        async: false,
-        datatype: "text/html",
-        data: {"backflag":$("#trialbaltype").val(),"accountcode":acccode,"calculatefrom":sessionStorage.yyyymmddyear1,"calculateto":newtodate,"financialstart":sessionStorage.yyyymmddyear1,"projectcode":"","monthlyflag":false,"narrationflag":false},
-        beforeSend: function(xhr)
-        {
-          xhr.setRequestHeader('gktoken',sessionStorage.gktoken );
-        },
-      })
-        .done(function(resp)
-        {
-          $("#info").html(resp);
-        }
-      );
+	$(".trialbaltable tbody tr").off('dblclick').on('dblclick',function(e){
+		e.preventDefault();
+		var acccode = $(this).attr('value');
+		if (acccode=="")
+		{
+				return false;
+		}
+		 var date = $("#ledtodate").val().split("-");
+		 var newtodate = date[2]+"-"+date[1]+"-"+date[0];
+		$.ajax(
+			{
+				type: "POST",
+				url: "/showledgerreport",
+				global: false,
+				async: false,
+				datatype: "text/html",
+				data: {"backflag":$("#trialbaltype").val(),"accountcode":acccode,"calculatefrom":sessionStorage.yyyymmddyear1,"calculateto":newtodate,"financialstart":sessionStorage.yyyymmddyear1,"projectcode":"","monthlyflag":false,"narrationflag":false},
+				beforeSend: function(xhr)
+				{
+					xhr.setRequestHeader('gktoken',sessionStorage.gktoken );
+				},
+			})
+				.done(function(resp)
+				{
+					$("#info").html(resp);
+				}
+			);
 
 
 
-  });
+	});
 
-  function trialbalcall(tbbaltype) {
-    $.ajax(
-      {
-        type: "POST",
-        url: "/showtrialbalancereport",
-        global: false,
-        async: false,
-        datatype: "text/html",
-        data: {"financialstart":sessionStorage.yyyymmddyear1,"calculateto":newtodate,"trialbalancetype":tbbaltype},
-        beforeSend: function(xhr)
-        {
-          xhr.setRequestHeader('gktoken',sessionStorage.gktoken );
-        }
-      })
-      .done(function(resp) {
-        $("#info").html(resp);
-      })
-      .fail(function() {
-        console.log("error");
-      })
-      .always(function() {
-        console.log("complete");
-      });
-  }
+	function trialbalcall(tbbaltype) {
+		$.ajax(
+			{
+				type: "POST",
+				url: "/showtrialbalancereport",
+				global: false,
+				async: false,
+				datatype: "text/html",
+				data: {"financialstart":sessionStorage.yyyymmddyear1,"calculateto":newtodate,"trialbalancetype":tbbaltype},
+				beforeSend: function(xhr)
+				{
+					xhr.setRequestHeader('gktoken',sessionStorage.gktoken );
+				}
+			})
+			.done(function(resp) {
+				$("#info").html(resp);
+			})
+			.fail(function() {
+				console.log("error");
+			})
+			.always(function() {
+				console.log("complete");
+			});
+	}
 
-  function open_in_newtab(filename, text) {
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:application/pdf;charset=utf-8,' +  encodeURIComponent(text));
-    element.setAttribute('target', '_blank');
-    element.style.display = 'none';
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
+	function open_in_newtab(filename, text) {
+		var element = document.createElement('a');
+		element.setAttribute('href', 'data:application/pdf;charset=utf-8,' +	encodeURIComponent(text));
+		element.setAttribute('target', '_blank');
+		element.style.display = 'none';
+		document.body.appendChild(element);
+		element.click();
+		document.body.removeChild(element);
  }
 
-  function printtrialbalance() {
-      Filename_net = "NetBalanceReport.pdf"
-      Filename_gross = "GrossBalanceReport.pdf"
-      Filename_ext = "ExtendedBalanceReport.pdf"
+	function printtrialbalance() {
+			Filename_net = "NetBalanceReport.pdf"
+			Filename_gross = "GrossBalanceReport.pdf"
+			Filename_ext = "ExtendedBalanceReport.pdf"
 
-      var orgname = sessionStorage.getItem('orgn');
-      var orgtype = sessionStorage.getItem('orgt');
-      var startyear = sessionStorage.getItem('year1');
-      var endyear = sessionStorage.getItem('year2');
+			var orgname = sessionStorage.getItem('orgn');
+			var orgtype = sessionStorage.getItem('orgt');
+			var startyear = sessionStorage.getItem('year1');
+			var endyear = sessionStorage.getItem('year2');
 
-      trialbalancetype = $("#trialbaltype").val();
-      $.ajax(
-        {
-          type: "GET",
-          url: "/printtrialbalance",
-          global: false,
-          async: false,
-          datatype: "text",
-          data: {"financialstart":sessionStorage.yyyymmddyear1, "calculateto":newtodate, "trialbalancetype": trialbalancetype, "orgname": orgname, "orgtype": orgtype, "startyear":startyear, "endyear":endyear},
-          beforeSend: function(xhr)
-          {
-            xhr.setRequestHeader('gktoken',sessionStorage.gktoken );
-          }
-        })
-        .done(function(resp) {
-          if(trialbalancetype == 1) {
-            open_in_newtab(Filename_net, resp);
-          }
-          else if(trialbalancetype == 2) {
-            open_in_newtab(Filename_gross, resp);
-          }
-          else if(trialbalancetype == 3) {
-            open_in_newtab(Filename_ext, resp);
-          }
-          console.log("done");
-        })
-        .fail(function() {
-          console.log("error");
-        })
-        .always(function() {
-          console.log("complete");
-        });
-  }
+			trialbalancetype = $("#trialbaltype").val();
+			$.ajax(
+				{
+					type: "GET",
+					url: "/printtrialbalance",
+					global: false,
+					async: false,
+					datatype: "text",
+					data: {"financialstart":sessionStorage.yyyymmddyear1, "calculateto":newtodate, "trialbalancetype": trialbalancetype, "orgname": orgname, "orgtype": orgtype, "startyear":startyear, "endyear":endyear},
+					beforeSend: function(xhr)
+					{
+						xhr.setRequestHeader('gktoken',sessionStorage.gktoken );
+					}
+				})
+				.done(function(resp) {
+					if(trialbalancetype == 1) {
+						open_in_newtab(Filename_net, resp);
+					}
+					else if(trialbalancetype == 2) {
+						open_in_newtab(Filename_gross, resp);
+					}
+					else if(trialbalancetype == 3) {
+						open_in_newtab(Filename_ext, resp);
+					}
+					console.log("done");
+				})
+				.fail(function() {
+					console.log("error");
+				})
+				.always(function() {
+					console.log("complete");
+				});
+	}
 
 
-  $("#ntbview").click(function(event) {
-    trialbalcall(1);
-  });
-  $("#gtbview").click(function(event) {
-    trialbalcall(2);
-  });
-  $("#extbview").click(function(event) {
-    trialbalcall(3);
-  });
-  $("#tbback").click(function(event) {
-    $("#showtrialbalance").click();
-  });
-  $("#printbutton").click(function(event) {
-    printtrialbalance();
-  });
+	$("#ntbview").click(function(event) {
+		trialbalcall(1);
+	});
+	$("#gtbview").click(function(event) {
+		trialbalcall(2);
+	});
+	$("#extbview").click(function(event) {
+		trialbalcall(3);
+	});
+	$("#tbback").click(function(event) {
+		$("#showtrialbalance").click();
+	});
+	$("#printbutton").click(function(event) {
+		printtrialbalance();
+	});
 
 });
