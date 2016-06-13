5b5b340b9d3373b92e013f94496998d6864fa9b9
Vaibhav Kurhe <vaibspidy@openmailbox.org>
2016-06-14 04:59:44 +0530

    Made some corrections: 1. Name and Financial Year will be displayed in bold on top on the first page, while in small font on all other pages. 2. Wide space was left on the top of the first page. Now it is reduced. 3. Changed the wording from CarriedForward to Carried Forward and same for Brought Forward. 4. Made the footers for all the pages in consistent layout. 5. Modified the table_style.

--- gkwebapp/views/cashflow.py
+++ gkwebapp/views/cashflow.py
@@ -43,6 +43,7 @@ from spreadsheettable import SpreadsheetTable
 from reportlab.lib.enums import TA_CENTER, TA_RIGHT
 from pyramid.response import Response
 import os
+import calendar
 from formula import TotalPagesColSum, PreviousPagesColSum
 
 
@@ -80,30 +81,40 @@ def printcashflowreport(request):
 	startyear = request.params["startyear"]
 	endyear = request.params["endyear"]
 	orgdata = orgname + " (" + orgtype + ")"
-	yeardata = "Financial Year : " + startyear + " to " + endyear
+	period = calculatefrom[8:10] + "-" + str(calendar.month_abbr[int(calculatefrom[5:7])]) + "-" + calculatefrom[0:4] + " to " + calculateto[8:10] + "-" +  str(calendar.month_abbr[int(calculateto[5:7])]) + "-" +  calculateto[0:4]
+	year = startyear[0:2] + "-" + str(calendar.month_abbr[int(startyear[3:5])]) + "-" + startyear[6:10] + " to " + endyear[0:2] + "-" +  str(calendar.month_abbr[int(endyear[3:5])]) + "-" +  endyear[6:10]
+	yeardata = "Financial Year: " + year
 
 	header={"gktoken":request.headers["gktoken"]}
 
 	if(orgtype == "Profit Making"):
-		CashFlow = "Cash Flow Account for the period " + calculatefrom + " to " + calculateto
+		CashFlow = "Cash Flow Account for the period " + period
 	elif(orgtype == "Not For Profit"):
-		CashFlow = "Receipt & Payment Account for the period " + calculatefrom + " to " + calculateto
+		CashFlow = "Receipt & Payment Account for the period " + period
 	pageinfo = "Cash Flow Report"
 	filename = "CashFlowReport.pdf"
 
 	def myFirstPage(canvas, doc):
 			canvas.saveState()
-			canvas.setFont('Times-Bold',16)
-			canvas.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-45, orgdata)
-			canvas.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-70, yeardata)
+			canvas.setFont('Times-Bold',15)
+			canvas.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-35, orgdata)
+			canvas.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-55, yeardata)
 			canvas.setFont('Times-Bold',13)
-			canvas.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-108, CashFlow)
+			canvas.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-80, CashFlow)
 			canvas.setFont('Times-Roman',9)
-			canvas.drawString(inch, 0.75 * inch, "First Page / %s" % pageinfo)
+			canvas.drawString(inch, 0.75 * inch, "Page %d %s" % (doc.page, pageinfo))
 			canvas.restoreState()
 
 	def myLaterPages(canvas, doc):
 			canvas.saveState()
+			canvas.setFont('Times-Bold',12)
+			canvas.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-40, orgname + "  FY: (" + year + ")")
+			canvas.setFont('Times-Roman',11)
+			if(orgtype == "Profit Making"):
+				canvas.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-55, "Cash Flow")
+			elif(orgtype == "Not For Profit"):
+				canvas.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-55, "Receipt & Payment")
+			canvas.drawCentredString(PAGE_WIDTH/2.0, PAGE_HEIGHT-70, period)
 			canvas.setFont('Times-Roman',9)
 			canvas.drawString(inch, 0.75 * inch, "Page %d %s" % (doc.page, pageinfo))
 			canvas.restoreState()
@@ -119,12 +130,12 @@ def printcashflowreport(request):
 		doc = SimpleDocTemplate(filename, pagesize=A4)
 		temp_list = [To, Particularsleft, Amtleft, By, Particularsright, Amtright]
 
-		Story = [Spacer(1,1*inch)]
+		Story = [Spacer(1,0.2*inch)]
 		data_json_left = result.json()["rcgkresult"]
 		data_json_right = result.json()["pygkresult"]
 		listoflist = []
 		listoflist.append(temp_list)
-		listoflist.append([Paragraph("", simplestyle), Paragraph("BroughtForward:", simplestyle), PreviousPagesColSum(decimal_places = 2), Paragraph("", simplestyle), Paragraph("BroughtForward:", simplestyle), PreviousPagesColSum(decimal_places = 2)])
+		listoflist.append([Paragraph("", simplestyle), Paragraph("Brought Forward:", simplestyle), PreviousPagesColSum(decimal_places = 2), Paragraph("", simplestyle), Paragraph("Brought Forward:", simplestyle), PreviousPagesColSum(decimal_places = 2)])
 
 
 		for entry in data_json_left:
@@ -134,7 +145,6 @@ def printcashflowreport(request):
 				particularsleft = entry["particulars"]
 				particularsleft = Paragraph(particularsleft, simplestyle)
 				amtleft = entry["amount"]
-				#amtleft = Paragraph(amtleft, simplestyle)
 				listoflist.append([to, particularsleft, amtleft])
 
 		i=2
@@ -145,20 +155,21 @@ def printcashflowreport(request):
 				particularsright = entryy["particulars"]
 				particularsright = Paragraph(particularsright, simplestyle)
 				amtright = entryy["amount"]
-				#amtright = Paragraph(amtright, simplestyle)
 				listoflist[i].append(by)
 				listoflist[i].append(particularsright)
 				listoflist[i].append(amtright)
 				i+=1
 		data = listoflist
 		table_style = [
-						('ALIGN', (0,0), (-1,-1), 'RIGHT'),
-						('INNERGRID', (0, 0), (6, 0), 0.25, colors.white),
-						('BOX', (0, 0), (-1, -1), 1, colors.black),
-						('BOX', (0, 0), (-1, 0), 1, colors.black),
-						('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+						('BACKGROUND', (0, 0), (-1, 0), '#a7a5a5'),
+						('ALIGN', (0, 0), (-1, -1), 'RIGHT'),
+						('GRID', (0, 0), (-1, 0), 1, colors.white),
+						('BOX', (0, 0), (-1, -1), 1, colors.grey),
+						('BOX', (0, 0), (-1, 0), 1, colors.grey),
+						('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+						('BOX', (0, 0), (-1, 0), 0.25, colors.black),
 		]
-		data.append([Paragraph("", simplestyle), Paragraph("CarriedForward:", simplestyle), TotalPagesColSum(decimal_places = 2), Paragraph("", simplestyle), Paragraph("CarriedForward:", simplestyle), TotalPagesColSum(decimal_places = 2)])
+		data.append([Paragraph("", simplestyle), Paragraph("Carried Forward:", simplestyle), TotalPagesColSum(decimal_places = 2), Paragraph("", simplestyle), Paragraph("Carried Forward:", simplestyle), TotalPagesColSum(decimal_places = 2)])
 		spreadsheet_table = SpreadsheetTable(data, repeatRows = 2, repeatRowsB = 1, colWidths = (1*cm, 5.2*cm, 3.8*cm, 1*cm, 5.2*cm, 3.8*cm))
 		spreadsheet_table.setStyle(table_style)
 
