1cf80e1c2fc60011c3d64ce9232df796e31e5061
Vanita Rajpurohit <vanita.rajpurohit9819@gmail.com>
2016-06-14 18:51:32 +0530

    minor changes in editorganisation

--- gkwebapp/views/organisation.py
+++ gkwebapp/views/organisation.py
@@ -7,7 +7,6 @@ from pyramid.renderers import render_to_response
 def showeditOrg(request):
 	header={"gktoken":request.headers["gktoken"]}
 	result = requests.get("http://127.0.0.1:6543/organisation", headers=header)
-	print result.json()["gkdata"]
 	return {"gkresult":result.json()["gkdata"],"gkstatus":result.json()["gkstatus"]}
 
 
@@ -16,7 +15,6 @@ def editOrganisation(request):
 	header={"gktoken":request.headers["gktoken"]}
 	gkdata= {"orgcity":request.params["orgcity"],"orgaddr":request.params["orgaddr"],"orgpincode":request.params["orgpincode"],"orgstate":request.params["orgstate"], "orgcountry":request.params["orgcountry"],"orgtelno":request.params["orgtelno"], "orgfax":request.params["orgfax"],"orgwebsite":request.params["orgwebsite"],"orgemail":request.params["orgemail"],"orgpan":request.params["orgpan"],"orgmvat":request.params["orgmvat"],"orgstax":request.params["orgstax"],"orgregno":request.params["orgregno"],"orgregdate":request.params["orgregdate"], "orgfcrano":request.params["orgfcrano"],"orgfcradate":request.params["orgfcradate"]}
 	result = requests.put("http://127.0.0.1:6543/organisations", headers=header, data=json.dumps(gkdata))
-	print "pqr"
 	return {"gkstatus":result.json()["gkstatus"]}
 
 @view_config(route_name="getorgcode", renderer="json")
