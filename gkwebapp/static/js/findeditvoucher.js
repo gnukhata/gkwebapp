36455a104c3dcb8825a2998cef8c8257eb5bc1b9
Vanita <vanita.rajpurohit9819@gmail.com>
2016-07-14 13:06:31 +0530

    Fixed bug in find and edit voucher

--- gkwebapp/static/js/findeditvoucher.js
+++ gkwebapp/static/js/findeditvoucher.js
@@ -260,57 +260,6 @@ $(document).ready(function() {
 
 $("#findvoucher").submit(function(event) {
 
-  var todate = $("#tyear").val()+$("#tmonth").val()+$("#tday").val();
-  var fromdate = $("#fyear").val()+$("#fmonth").val()+$("#fday").val();
-  var fstart = Date.parseExact(sessionStorage.yyyymmddyear1,"yyyy-MM-dd");
-  var fend = Date.parseExact(sessionStorage.yyyymmddyear2,"yyyy-MM-dd");
-  if (!Date.parseExact(todate,"yyyyMMdd"))
-  {
-    $("#improperdate-alert").alert();
-    $("#improperdate-alert").fadeTo(2250, 400).slideUp(500, function(){
-      $("#improperdate-alert").hide();
-    });
-    $("#tday").focus();
-    $("#tday").select();
-    return false;
-  };
-  if (!Date.parseExact(fromdate,"yyyyMMdd"))
-  {
-    $("#improperdate-alert").alert();
-    $("#improperdate-alert").fadeTo(2250, 400).slideUp(500, function(){
-      $("#improperdate-alert").hide();
-    });
-    $("#fday").focus();
-    $("#fday").select();
-    return false;
-  };
-  if (!Date.parseExact(todate,"yyyyMMdd").between(fstart,fend))
-  {
-    $("#betweendate-alert").alert();
-    $("#betweendate-alert").fadeTo(2250, 400).slideUp(500, function(){
-      $("#betweendate-alert").hide();
-    });
-
-    $("#tday").focus();
-    $("#tday").select();
-    return false;
-
-  }
-
-  if (!Date.parseExact(fromdate,"yyyyMMdd").between(fstart,fend))
-  {
-    $("#betweendate-alert").alert();
-    $("#betweendate-alert").fadeTo(2250, 400).slideUp(500, function(){
-      $("#betweendate-alert").hide();
-    });
-
-    $("#fday").focus();
-    $("#fday").select();
-    return false;
-
-  }
-
-
   var search = $("#searchby option:selected").val();
   if (search=="vnum")
   {
@@ -338,6 +287,13 @@ $("#findvoucher").submit(function(event) {
   }
   else if (search=="date")
   {
+
+
+    var todate = $("#tyear").val()+$("#tmonth").val()+$("#tday").val();
+    var fromdate = $("#fyear").val()+$("#fmonth").val()+$("#fday").val();
+    var fstart = Date.parseExact(sessionStorage.yyyymmddyear1,"yyyy-MM-dd");
+    var fend = Date.parseExact(sessionStorage.yyyymmddyear2,"yyyy-MM-dd");
+
     if ($('#fday').val()=="") {
       $("#vdt-alert").alert();
       $("#nodt").focus();
@@ -397,6 +353,51 @@ $("#findvoucher").submit(function(event) {
       return false;
     }
 
+    if (!Date.parseExact(todate,"yyyyMMdd"))
+    {
+      $("#improperdate-alert").alert();
+      $("#improperdate-alert").fadeTo(2250, 400).slideUp(500, function(){
+        $("#improperdate-alert").hide();
+      });
+      $("#tday").focus();
+      $("#tday").select();
+      return false;
+    };
+    if (!Date.parseExact(fromdate,"yyyyMMdd"))
+    {
+      $("#improperdate-alert").alert();
+      $("#improperdate-alert").fadeTo(2250, 400).slideUp(500, function(){
+        $("#improperdate-alert").hide();
+      });
+      $("#fday").focus();
+      $("#fday").select();
+      return false;
+    };
+    if (!Date.parseExact(todate,"yyyyMMdd").between(fstart,fend))
+    {
+      $("#betweendate-alert").alert();
+      $("#betweendate-alert").fadeTo(2250, 400).slideUp(500, function(){
+        $("#betweendate-alert").hide();
+      });
+
+      $("#tday").focus();
+      $("#tday").select();
+      return false;
+
+    }
+
+    if (!Date.parseExact(fromdate,"yyyyMMdd").between(fstart,fend))
+    {
+      $("#betweendate-alert").alert();
+      $("#betweendate-alert").fadeTo(2250, 400).slideUp(500, function(){
+        $("#betweendate-alert").hide();
+      });
+
+      $("#fday").focus();
+      $("#fday").select();
+      return false;
+
+    }
   }
 
   else if (search=="narration")
