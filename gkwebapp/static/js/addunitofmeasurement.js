43f3e1410bdb96ce463548b99df3316667a4de92
sach311 <sachin619patil@rediffmail.com>
2016-11-05 19:09:51 +0530

    created UI for Adding unit in Unit of Measurement

---
+++ gkwebapp/static/js/addunitofmeasurement.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+  $('#unit_name').focus();
+  $("#conversion_rate").numeric();
+
+
+  $("#unit_name").keydown(function(event) {
+    if (event.which==13) {
+      event.preventDefault();
+      $("#sub_unit_of").focus().select();
+    }
+  });
+
+  $("#sub_unit_of").keydown(function(event) {
+    if (event.which==13 && $("#sub_unit_of option:selected").val()=='') {
+      event.preventDefault();
+      $("#unit_save").click();
+    }
+    else if(event.which==13 && $("#sub_unit_of option:selected").val()!='') {
+      event.preventDefault();
+      $("#conversion_rate").focus().select();
+
+    }
+    if (event.which==38 && $("#sub_unit_of option:selected").index()==0) {
+      event.preventDefault();
+      $("#unit_name").focus().select();
+    }
+  });
+  $("#conversion_rate").keydown(function(event) {
+    if (event.which==13){
+      event.preventDefault();
+      $("#unit_save").click();
+    }
+    if (event.which==38){
+      event.preventDefault();
+      $("#sub_unit_of").focus().select();
+    }
+  });
+
+  $("#sub_unit_of").change(function(event) {
+    if ($("#sub_unit_of option:selected").val()!='') {
+      $("#conversion_div").show();
+    }
+    else  {
+      $("#conversion_div").hide();
+    }
+  });
+  $("#unit_save").click(function(event) {
+    if ($.trim($('#unit_name').val())=="") {
+      $("#unit-blank-alert").alert();
+      $("#unit-blank-alert").fadeTo(2250, 500).slideUp(500, function(){
+        $("#unit-blank-alert").hide();
+      });
+      $('#unit_name').focus().select();
+      return false;
+    }
+    $.ajax({
+      url: '/unitofmeasurements?action=save',
+      type: 'POST',
+      dataType: 'json',
+      async : false,
+      data: {"unitname": $("#unit_name").val(),"conversionrate":$("#conversion_rate").val(),"subunitof":$("#sub_unit_of option:selected").val()},
+      beforeSend: function(xhr)
+      {
+        xhr.setRequestHeader('gktoken', sessionStorage.gktoken);
+      }
+    })
+    .done(function(resp) {
+      if(resp["gkstatus"] == 0){
+        $("a[href='#unit_create']").click();
+        $("#success-alert").alert();
+        $("#success-alert").fadeTo(2250, 500).slideUp(500, function(){
+          $("#success-alert").hide();
+        });
+        return false;
+      }
+      else if(resp["gkstatus"] == 1){
+        $("a[href='#unit_create']").click();
+        $("#duplicate-alert").alert();
+        $("#duplicate-alert").fadeTo(2250, 500).slideUp(500, function(){
+          $("duplicate-alert").hide();
+        });
+        return false;
+      }
+      else {
+        $("#unit_name").focus();
+        $("#failure-alert").alert();
+        $("#failure-alert").fadeTo(2250, 500).slideUp(500, function(){
+          $("#failure-alert").hide();
+        });
+        return false;
+      }
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+    });
+
+  });
+});
