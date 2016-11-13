f8360cfbd9a25edfc816fe1df2fde631224a8758
ishan masdekar <imasdekar@dff.org.in>
2016-11-13 16:50:55 +0530

    Now state is by default selected when user selects delivery challan in invoice . Also working on edit invoice.

--- gkwebapp/views/invoice.py
+++ gkwebapp/views/invoice.py
@@ -103,5 +103,4 @@ def showeditinvoice(request):
 def getInvoiceDetails(request):
 	header={"gktoken":request.headers["gktoken"]}
 	invoicedata = requests.get("http://127.0.0.1:6543/invoice?inv=single&invid=%d"%(int(request.params["invid"])), headers=header)
-
 	return {"gkstatus": invoicedata.json()["gkstatus"],"invoicedata": invoicedata.json()["gkresult"]}
