a96d74d1d4e90a0e54dc4494a189aaf55c58042b
abhi1tb <abhijithb21@openmailbox.org>
2016-11-29 00:14:16 +0530

    Tree view is almost complete. Needs accessibility improvements.

--- gkwebapp/static/js/treeviewofcategories.js
+++ gkwebapp/static/js/treeviewofcategories.js
@@ -6,30 +6,32 @@ $(document).ready(function() {
     event.preventDefault();
     var indextp = $(".topparent").index(this);
     var categorycode = $(".topparent").eq(indextp).next(".categorycode").val();
-    console.log($(this).closest("li").length);
-    console.log($(this).closest("li").children().length);
     if ($(this).closest("li").children().length > 2) {
       $(this).closest("li").children("li").toggle();
     }
     else {
-      $.ajax({
-        url: '/category?action=treechildren',
-        type: 'POST',
-        global: false,
-        async: false,
-        datatype: 'json',
-        data:{"categorycode":categorycode},
-        beforeSend: function(xhr)
-        {
-          xhr.setRequestHeader('gktoken', sessionStorage.gktoken);
-        }
-      })
-      .done(function(resp){
-        var childrenofparent = resp["gkresult"];
-        for (i in childrenofparent) {
-          $(".topparent").eq(indextp).parent("li").append('<li style="margin-left:20px;"><a href="" class="topparent"><span class="glyphicon glyphicon-triangle-right"></span>'+childrenofparent[i].categoryname+'</a><input type="text" class="categorycode" name="name" value='+childrenofparent[i].categorycode+' hidden></li>');
-        }
-      });
+      if ($(this).children(".badge").text()!="0") {
+        $(this).children(".glyphicon-triangle-bottom").toggle();
+        $(this).children(".glyphicon-triangle-right").toggle();
+        $.ajax({
+          url: '/category?action=treechildren',
+          type: 'POST',
+          global: false,
+          async: false,
+          datatype: 'json',
+          data:{"categorycode":categorycode},
+          beforeSend: function(xhr)
+          {
+            xhr.setRequestHeader('gktoken', sessionStorage.gktoken);
+          }
+        })
+        .done(function(resp){
+          var childrenofparent = resp["gkresult"];
+          for (i in childrenofparent) {
+            $(".topparent").eq(indextp).parent("li").append('<li class="list-group-item list-group-item-primary" style="margin-left:20px;"><a href="" class="topparent list-group-item list-group-item-primary"><span class="glyphicon glyphicon-triangle-right"></span><span class="glyphicon glyphicon-triangle-bottom" style="display: none;"></span>'+childrenofparent[i].categoryname+'<span class="badge">'+childrenofparent[i].subcount+'</span></a><input type="text" class="categorycode" name="name" value='+childrenofparent[i].categorycode+' hidden></li>');
+          }
+        });
+      }
     }
   });
 });
