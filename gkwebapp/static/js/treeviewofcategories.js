bba23cc39470a4db89bfb3cfd4cd6cfed6c60b75
abhi1tb <abhijithb21@openmailbox.org>
2016-11-28 19:16:39 +0530

    Toggling works in tree view

--- gkwebapp/static/js/treeviewofcategories.js
+++ gkwebapp/static/js/treeviewofcategories.js
@@ -5,26 +5,31 @@ $(document).ready(function() {
   $(document).off("click",".topparent").on("click", ".topparent", function(event){
     event.preventDefault();
     var indextp = $(".topparent").index(this);
-    console.log($(".topparent").index(this));
     var categorycode = $(".topparent").eq(indextp).next(".categorycode").val();
-    console.log(categorycode);
-    $.ajax({
-      url: '/category?action=treechildren',
-      type: 'POST',
-      global: false,
-      async: false,
-      datatype: 'json',
-      data:{"categorycode":categorycode},
-      beforeSend: function(xhr)
-      {
-        xhr.setRequestHeader('gktoken', sessionStorage.gktoken);
-      }
-    })
-    .done(function(resp){
-      var childrenofparent = resp["gkresult"];
-      for (i in childrenofparent) {
-        $(".topparent").eq(indextp).parent("li").append('<li style="margin-left:20px;"><a href="" class="topparent"><span class="glyphicon glyphicon-triangle-right"></span>'+childrenofparent[i].categoryname+'</a><input type="text" class="categorycode" name="name" value='+childrenofparent[i].categorycode+' hidden></li>');
-      }
-    });
+    console.log($(this).closest("li").length);
+    console.log($(this).closest("li").children().length);
+    if ($(this).closest("li").children().length > 2) {
+      $(this).closest("li").children("li").toggle();
+    }
+    else {
+      $.ajax({
+        url: '/category?action=treechildren',
+        type: 'POST',
+        global: false,
+        async: false,
+        datatype: 'json',
+        data:{"categorycode":categorycode},
+        beforeSend: function(xhr)
+        {
+          xhr.setRequestHeader('gktoken', sessionStorage.gktoken);
+        }
+      })
+      .done(function(resp){
+        var childrenofparent = resp["gkresult"];
+        for (i in childrenofparent) {
+          $(".topparent").eq(indextp).parent("li").append('<li style="margin-left:20px;"><a href="" class="topparent"><span class="glyphicon glyphicon-triangle-right"></span>'+childrenofparent[i].categoryname+'</a><input type="text" class="categorycode" name="name" value='+childrenofparent[i].categorycode+' hidden></li>');
+        }
+      });
+    }
   });
 });
