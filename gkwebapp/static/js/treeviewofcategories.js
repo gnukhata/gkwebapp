c48b82b46ff39bdac8896735079876a526dad710
abhi1tb <abhijithb21@openmailbox.org>
2016-11-29 11:04:47 +0530

    Improved accessibility of tree view of categories

--- gkwebapp/static/js/treeviewofcategories.js
+++ gkwebapp/static/js/treeviewofcategories.js
@@ -7,10 +7,16 @@ $(document).ready(function() {
     var indextp = $(".topparent").index(this);
     var categorycode = $(".topparent").eq(indextp).next(".categorycode").val();
     if ($(this).closest("li").children().length > 2) {
+      $(this).attr('aria-expanded', 'false');
+      $(this).blur();
+      $(".topparent").eq(indextp).focus();
       $(this).closest("li").children("li").toggle();
     }
     else {
       if ($(this).children(".badge").text()!="0") {
+        $(this).attr('aria-expanded', 'true');
+        $(this).blur();
+        $(".topparent").eq(indextp).focus();
         $(this).children(".glyphicon-triangle-bottom").toggle();
         $(this).children(".glyphicon-triangle-right").toggle();
         $.ajax({
@@ -28,7 +34,7 @@ $(document).ready(function() {
         .done(function(resp){
           var childrenofparent = resp["gkresult"];
           for (i in childrenofparent) {
-            $(".topparent").eq(indextp).parent("li").append('<li class="list-group-item list-group-item-primary" style="margin-left:20px;"><a href="" class="topparent list-group-item list-group-item-primary"><span class="glyphicon glyphicon-triangle-right"></span><span class="glyphicon glyphicon-triangle-bottom" style="display: none;"></span>'+childrenofparent[i].categoryname+'<span class="badge">'+childrenofparent[i].subcount+'</span></a><input type="text" class="categorycode" name="name" value='+childrenofparent[i].categorycode+' hidden></li>');
+            $(".topparent").eq(indextp).parent("li").append('<li class="list-group-item list-group-item-primary" style="margin-left:20px;"><a href="" class="topparent list-group-item list-group-item-primary" aria-expanded="false"><span class="glyphicon glyphicon-triangle-right"></span><span class="glyphicon glyphicon-triangle-bottom" style="display: none;"></span>'+childrenofparent[i].categoryname+'<span class="sr-only">has</span><span class="badge">'+childrenofparent[i].subcount+'</span><span class="sr-only"> sub-categories</span></a><input type="text" class="categorycode" name="name" value='+childrenofparent[i].categorycode+' hidden></li>');
           }
         });
       }
