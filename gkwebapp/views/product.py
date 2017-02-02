b90e020982577d89ab64af2df73fd838db3bae16
Talhapawaty <mtalha456@gmail.com>
2017-02-02 14:41:08 +0530

    issue #219 and issue #193 is resolved.

--- gkwebapp/views/product.py
+++ gkwebapp/views/product.py
@@ -419,13 +419,13 @@ def stockreportspreadsheet(request):
 			row += 1
 	else:
 		sheet.getCell(0,0).stringValue(orgname).setBold(True).setAlignHorizontal("center").setFontSize("16pt")
-		ods.content.mergeCells(0,0,9,1)
+		ods.content.mergeCells(0,0,8,1)
 		sheet.getRow(1).setHeight("18pt")
 		sheet.getCell(0,1).stringValue("Product Report (Period : "+calculatefrom+" to "+calculateto+")").setBold(True).setFontSize("14pt").setAlignHorizontal("center")
-		ods.content.mergeCells(0,1,9,1)
+		ods.content.mergeCells(0,1,8,1)
 		sheet.getRow(2).setHeight("16pt")
 		sheet.getCell(0,2).stringValue("Name of the Product: "+productdesc).setBold(True).setFontSize("14pt").setAlignHorizontal("center")
-		ods.content.mergeCells(0,2,9,1)
+		ods.content.mergeCells(0,2,8,1)
 		sheet.getColumn(1).setWidth("4cm")
 		sheet.getColumn(2).setWidth("5cm")
 		sheet.getColumn(3).setWidth("4cm")
