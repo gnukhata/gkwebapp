0a01968df9ce59566fd57f560f4144213083a9e8
Pornima <pornima@openmailbox.org>
2017-02-04 09:55:45 +0530

    changes in merge cells spreadsheet and in print view column widths

--- gkwebapp/views/godown.py
+++ gkwebapp/views/godown.py
@@ -137,10 +137,10 @@ def listofgodownssspreadsheet(request):
     sheet.getRow(0).setHeight("23pt")
 
     sheet.getCell(0,0).stringValue(orgname).setBold(True).setAlignHorizontal("center").setFontSize("16pt")
-    ods.content.mergeCells(0,0,4,1)
+    ods.content.mergeCells(0,0,7,1)
     sheet.getRow(1).setHeight("18pt")
     sheet.getCell(0,1).stringValue("List Of Godowns").setBold(True).setFontSize("14pt").setAlignHorizontal("center")
-    ods.content.mergeCells(0,1,4,1)
+    ods.content.mergeCells(0,1,7,1)
     sheet.getColumn(1).setWidth("4cm")
     sheet.getColumn(2).setWidth("8cm")
     sheet.getColumn(3).setWidth("4cm")
