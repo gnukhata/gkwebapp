02afecfde3f2598abe5e24635a692d095eecc57d
Bhavesh <bbhavesh07@gmail.com>
2017-02-18 15:56:55 +0530

    while adding voucher if organization is profit making then istead project it is changed to Cost center

--- gkwebapp/static/js/addvoucher.js
+++ gkwebapp/static/js/addvoucher.js
@@ -48,6 +48,9 @@ $(document).ready(function() {
   {
     $(".invhide").show();
   }
+  if (sessionStorage.orgt=="Profit Making") {
+    $("label[for='project']").text("Cost Center:");
+  }
   $("#vno").focus().select();
   $('.vdate').autotab('number');    //autotab is a library for automatically switching the focus to next input when max allowed characters are filled.
   $('.dramt').numeric({ negative: false });   //numeric is a library used for restricting the user to input only numbers and decimal inside a text box
