372584ce64a9c3ec262e0e90d108ef698bc3b9d2
Prajkta_Patkar <prajkta.patkar007@gmail.com>
2017-03-17 17:13:08 +0530

    edir cust and supp : change to text box from drop down list

--- gkwebapp/static/js/editcustomersupplier.js
+++ gkwebapp/static/js/editcustomersupplier.js
@@ -19,6 +19,13 @@ $(document).ready(function() {
       var result = resp["gkresult"];
       $(".hidden-load").show();
       $("#edit_cussup").val(result["csflag"]);
+      if(result["csflag"] == 3){
+    	  
+    	  $("#edit_cussup").val("Customer");
+      }
+      else {
+    	  $("#edit_cussup").val("Supplier");
+      }
       $("#edit_cussup").prop("disabled", true);
       $("#edit_cussup_name").val(result["custname"]);
       $("#edit_cussup_name").prop("disabled", true);
@@ -85,10 +92,7 @@ $(document).ready(function() {
       event.preventDefault();
       $("#edit_cussup_email").focus().select();
     }
-  /*  if (event.which==38){
-      event.preventDefault();
-      $("#edit_cussup").focus().select();
-    }*/
+  
   });
   $("#edit_cussup_email").keydown(function(event) {
     if (event.which==13) {
