63b445fe2799b90086147a338dadf8ac22f291c3
robaraskar <robaraskar@gmail.com>
2017-03-18 18:57:18 +0530

    supplier/customer address, state and tin are now auto filled by either selecting delivery note or by selecting supplier/customer.

--- gkwebapp/views/customerandsupplier.py
+++ gkwebapp/views/customerandsupplier.py
@@ -47,7 +47,7 @@ def showaddcustomersupplier(request):
 @view_config(route_name="customersuppliers",request_param="action=showaddpopup",renderer="gkwebapp:templates/createcustsuppopup.jinja2")
 def showaddcustomersupplierpopup(request):
 	header={"gktoken":request.headers["gktoken"]}
-	
+
 	customers = requests.get("http://127.0.0.1:6543/customersupplier?qty=custall", headers=header)
 	suppliers = requests.get("http://127.0.0.1:6543/customersupplier?qty=supall", headers=header)
 	groups = requests.get("http://127.0.0.1:6543/groupsubgroups?groupflatlist", headers=header)
@@ -85,7 +85,7 @@ def savecustomersupplier(request):
 def editcustomersupplier(request):
 	header={"gktoken":request.headers["gktoken"]}
 #	dataset={"custname":request.params["custname"],"custaddr":request.params["custaddr"],"custphone":request.params["custphone"],"custemail":request.params["custemail"],"custfax":request.params["custfax"],"custpan":request.params["custpan"],"state":request.params["state"],"custtan":request.params["custtan"],"csflag":int(request.params["csflag"]),"custid":int(request.params["custid"])}
-        dataset={"custname":request.params["custname"],"custaddr":request.params["custaddr"],"custphone":request.params["custphone"],"custemail":request.params["custemail"],"custfax":request.params["custfax"],"custpan":request.params["custpan"],"state":request.params["state"],"custtan":request.params["custtan"],"custid":int(request.params["custid"])}
+	dataset={"custname":request.params["custname"],"custaddr":request.params["custaddr"],"custphone":request.params["custphone"],"custemail":request.params["custemail"],"custfax":request.params["custfax"],"custpan":request.params["custpan"],"state":request.params["state"],"custtan":request.params["custtan"],"custid":int(request.params["custid"])}
 	result=requests.put("http://127.0.0.1:6543/customersupplier",data=json.dumps(dataset),headers=header)
 	return {"gkstatus": result.json()["gkstatus"]}
 
