b887b447be14808448102a9dd59b8759cb636c3e
Pornima <pornima@openmailbox.org>
2017-03-22 15:39:54 +0530

    changed position of click event

--- gkwebapp/static/js/viewpurchaseorderdetails.js
+++ gkwebapp/static/js/viewpurchaseorderdetails.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+
+    $(document).off('click', '.purchaseorder_schedule').on('click', '.purchaseorder_schedule', function(event) {
+      var curindex1 = $(this).closest('tr').index();
+      pcode1 = $("#purchaseorder_product_table tbody tr:eq("+curindex1+") td:eq(0) select option:selected").val();
+       $("#view_schedule_table  tbody tr").each(function(){
+          if($(".procode",this).val() != pcode1){
+            $(this).hide();
+          }
+          else {
+            $(this).show();
+          }
+       });
+    });
+
   $("#viewpomodal").on('shown.bs.modal', function(event) {
     $("#purchaseorder_product_table  tbody tr").each(function(){
       var curindex = $(this).closest('tr').index();
@@ -32,19 +46,6 @@ $(document).ready(function() {
   });
   $("#viewpomodal").on('hidden.bs.modal', function(event) {
 
-  });
-
-  $(document).off('click', '.purchaseorder_schedule').on('click', '.purchaseorder_schedule', function(event) {
-    var curindex1 = $(this).closest('tr').index();
-    pcode1 = $("#purchaseorder_product_table tbody tr:eq("+curindex1+") td:eq(0) select option:selected").val();
-     $("#view_schedule_table  tbody tr").each(function(){
-        if($(".procode",this).val() != pcode1){
-          $(this).hide();
-        }
-        else {
-          $(this).show();
-        }
-     });
   });
 
 
