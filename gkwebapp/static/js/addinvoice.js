503dc45899d481bf05777064c207a2e7a9ab2834
Prajkta Patkar <prajkta.patkar007@gmail.com>
2017-03-23 13:40:31 +0530

    Button style changed in create godown for LIst of all godown

--- gkwebapp/static/js/addinvoice.js
+++ gkwebapp/static/js/addinvoice.js
@@ -22,6 +22,7 @@ Contributors:
 "Krishnakant Mane" <kk@gmail.com>
 "Ishan Masdekar " <imasdekar@dff.org.in>
 "Navin Karkera" <navin@dff.org.in>
+"Prajkta Patkar"<prajkta.patkar007@gmail.com>
 */
 
 // This script is for the addinvoice.jinja2
