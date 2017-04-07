dbd988d1b0623c2a6599a774ebbe54c7a221577d
robaraskar <robaraskar@gmail.com>
2017-04-07 16:39:20 +0530

    changes in edit category.

--- gkwebapp/static/js/editcategory.js
+++ gkwebapp/static/js/editcategory.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
             {
               $("#category_edit_innerdiv").hide();
                 $(".panel-footer").hide();
-              
+
               $("#cant-edit-alert").alert();
               $("#cant-edit-alert").fadeTo(2250, 500).slideUp(500, function(){
                 $("#cant-edit-alert").hide();
@@ -47,7 +47,7 @@ $(document).ready(function() {
             else {
               $("#category_edit_innerdiv").show();
               $(".panel-footer").show();
-              
+
             }
           })
           .fail(function() {
@@ -331,6 +331,30 @@ $(document).ready(function() {
       $("#category_edit_tax_table tbody tr:eq("+curindex+") td:eq(1) select").empty();
       $("#category_edit_tax_table tbody tr:eq("+curindex+") td:eq(1) select").append('<option value="">None</option>');
     }
+    var curindex = $(this).closest('tr').index();
+    var previndex = curindex -1;
+    if ($("#category_edit_tax_table tbody tr:eq("+curindex+") td:eq(0) select").val()=='VAT') {
+      $("#category_edit_tax_table tbody tr:eq("+curindex+") td:eq(1) select").empty();
+      $("#category_edit_tax_table tbody tr:eq("+curindex+") td:eq(1) select").append('<option value="" stateid="0" hidden disabled selected>Select State</option><option value="Andaman and Nicobar Islands" stateid="1">Andaman and Nicobar Islands</option><option value="Andhra Pradesh" stateid="2">Andhra Pradesh</option><option value="Arunachal Pradesh" stateid="3">Arunachal Pradesh</option><option value="Assam" stateid="4">Assam</option><option value="Bihar" stateid="5">Bihar</option><option value="Chandigarh" stateid="6">Chandigarh</option><option value="Chhattisgarh" stateid="7">Chhattisgarh</option><option value="Dadra and Nagar Haveli" stateid="8">Dadra and Nagar Haveli</option><option value="Daman and Diu" stateid="9">Daman and Diu</option><option value="Delhi" stateid="10">Delhi</option><option value="Goa" stateid="11">Goa</option><option value="Gujarat" stateid="12">Gujarat</option><option value="Haryana" stateid="13">Haryana</option><option value="Himachal Pradesh" stateid="14">Himachal Pradesh</option><option value="Jammu and Kashmir" stateid="15">Jammu and Kashmir</option><option value="Jharkhand" stateid="16">Jharkhand</option><option value="Karnataka" stateid="17">Karnataka</option><option value="Kerala" stateid="19">Kerala</option><option value="Lakshadweep" stateid="20">Lakshadweep</option><option value="Madhya Pradesh" stateid="21">Madhya Pradesh</option><option value="Maharashtra" stateid="22">Maharashtra</option><option value="Manipur" stateid="23">Manipur</option><option value="Meghalaya" stateid="24">Meghalaya</option><option value="Mizoram" stateid="25">Mizoram</option><option value="Nagaland" stateid="26">Nagaland</option><option value="Odisha" stateid="29">Odisha</option><option value="Pondicherry" stateid="31">Pondicherry</option><option value="Punjab" stateid="32">Punjab</option><option value="Rajasthan" stateid="33">Rajasthan</option><option value="Sikkim" stateid="34">Sikkim</option><option value="Tamil Nadu" stateid="35">Tamil Nadu</option><option value="Telangana" stateid="36">Telangana</option><option value="Tripura" stateid="37">Tripura</option><option value="Uttar Pradesh" stateid="38">Uttar Pradesh</option><option value="Uttarakhand" stateid="39">Uttarakhand</option><option value="West Bengal" stateid="41">West Bengal</option>');
+      if (curindex > 0) {
+        for (var i = 1; i < curindex+1; i++) {
+          for (var j = 0; j < curindex; j++) {
+            if ($("#category_edit_tax_table tbody tr:eq("+i+") td:eq(0) select").val() == "CVAT") {
+              i = i + 1;
+            }
+            selectedtaxstate = $("#category_edit_tax_table tbody tr:eq("+j+") td:eq(1) select option:selected").attr("stateid");
+  console.log(selectedtaxstate);
+            $('#category_edit_tax_table tbody tr:eq('+i+') td:eq(1) select option[stateid='+selectedtaxstate+']').prop('hidden', true).prop('disabled', true);
+          }
+        }
+      }
+    }
+    else {
+      $("#category_edit_tax_table tbody tr:eq("+curindex+") td:eq(1) select").empty();
+      $("#category_edit_tax_table tbody tr:eq("+curindex+") td:eq(1) select").append('<option value="">None</option>');
+    }
+    selectedtaxname = $("#category_edit_tax_table tbody tr:eq("+curindex+") td:eq(0) select").val();
+
   });
 
   $(document).off("keydown",".tax_state").on("keydown",".tax_state",function(event)
