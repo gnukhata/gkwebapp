a084252c80165422ba89d048094eb48a1f44c549
abhi1tb <abhijithb21@openmailbox.org>
2017-04-13 12:27:17 +0530

    Now Delivery Note In and Delivery Note Out are listed separately. Print statement while viewing delivery challan removed

--- gkwebapp/views/deliverychallan.py
+++ gkwebapp/views/deliverychallan.py
@@ -41,7 +41,7 @@ def showdeliverychallan(request):
 	delchals = requests.get("http://127.0.0.1:6543/delchal?delchal=all", headers=header)
 	return {"numberofproducts":len(delchals.json()["gkresult"]),"status":True}
 
-	
+
 
 @view_config(route_name="deliverychallan",request_param="action=showadd",renderer="gkwebapp:templates/adddeliverychallan.jinja2")
 def showadddeliverychallan(request):
@@ -64,13 +64,12 @@ def showadddeliverychallan(request):
 
 @view_config(route_name="deliverychallan",request_param="action=showedit",renderer="gkwebapp:templates/editdeliverychallan.jinja2")
 def showeditdeliverychallan(request):
-	
+
 	header={"gktoken":request.headers["gktoken"]}
 	delchals = requests.get("http://127.0.0.1:6543/delchal?delchal=all", headers=header)
 	suppliers = requests.get("http://127.0.0.1:6543/customersupplier?qty=supall", headers=header)
 	customers = requests.get("http://127.0.0.1:6543/customersupplier?qty=custall", headers=header)
 	godowns = requests.get("http://127.0.0.1:6543/godown", headers=header)
-	print len(delchals.json()["gkresult"])
 	return {"gkstatus":delchals.json()["gkstatus"],"delchals":delchals.json()["gkresult"],"suppliers":suppliers.json()["gkresult"],"customers":customers.json()["gkresult"],"godowns":godowns.json()["gkresult"]}
 
 @view_config(route_name="deliverychallan", request_param="action=showeditpopup", renderer="gkwebapp:templates/editdeliverychallanpopup.jinja2")
