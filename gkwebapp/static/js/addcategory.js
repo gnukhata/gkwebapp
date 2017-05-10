d1397bfada61d29f10e26baa80a736cdbb53747c
abhi1tb <abhijithb21@openmailbox.org>
2017-05-10 16:28:54 +0530

    Added contributor's name in addcategory.js

--- gkwebapp/static/js/addcategory.js
+++ gkwebapp/static/js/addcategory.js
@@ -1,27 +1,35 @@
 /*
-Copyright (C) 2013, 2014, 2015, 2016 Digital Freedom Foundation
-This file is part of GNUKhata:A modular,robust and Free Accounting System.
+   Copyright (C) 2013, 2014, 2015, 2016 Digital Freedom Foundation
+   This file is part of GNUKhata:A modular,robust and Free Accounting System.
 
-GNUKhata is Free Software; you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation; either version 3 of
-the License, or (at your option) any later version.
+   GNUKhata is Free Software; you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as
+   published by the Free Software Foundation; either version 3 of
+   the License, or (at your option) any later version.
 
-GNUKhata is distributed in the hope that it will be useful, but
-WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
+   GNUKhata is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU Affero General Public
-License along with GNUKhata (COPYING); if not, write to the
-Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-Boston, MA  02110-1301  USA59 Temple Place, Suite 330,
+   You should have received a copy of the GNU Affero General Public
+   License along with GNUKhata (COPYING); if not, write to the
+   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA  02110-1301  USA59 Temple Place, Suite 330,
 
 
-Contributors:
-"Bhavesh Bawadhane" <bbhavesh07@gmail.com>
-"Rohini Baraskar"
-*/
+   Contributors:
+   "Abhijith Balan" <abhijithb21@openmailbox.org>
+   "Bhavesh Bawadhane" <bbhavesh07@gmail.com>
+   "Ishan Masdekar" <imasdekar@dff.org.in>
+   "Mohd. Talha Pawaty" <mtalha456@gmail.com>
+   "Navin Karkera" <navin@openmailbox.org>
+   "Pornima Kolte" <pornima@openmailbox.org>
+   "Rohini Baraskar" <robaraskar@gmail.com>
+   "Sachin Patil" <sachin619patil@rediffmail.com>
+   "Vaibhav Kurhe" <vaibspidy@openmailbox.org>
+   
+ */
 // This script is for the add category page
 $(document).ready(function() {
     $('.modal-backdrop').remove();
