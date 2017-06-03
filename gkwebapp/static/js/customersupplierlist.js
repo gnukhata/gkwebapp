8193bc0f61f8bb7278c0192db72613080ecbaa43
abhi1tb <abhijithb21@openmailbox.org>
2017-06-03 15:20:36 +0530

    Added documentation in customersupplierlist.js

--- gkwebapp/static/js/customersupplierlist.js
+++ gkwebapp/static/js/customersupplierlist.js
@@ -21,9 +21,20 @@
    Contributors:
    "Krishnakant Mane" <kk@gmail.com>
    "Abhijith Balan" <abhijithb21@openmailbox.org.in>
- */
+*/
+
+//This is a script for the template showing list of customers and suppliers for unadjusted amounts module.
+
+//Events that take place as soon as the template is loaded.
 $(document).ready(function() {
+  //When the page is ready focus must be on the first select field.
   $("select:first").focus();
+  //Actions that happen after change in the list of customers. This implies a customer or supplier has been selected.
+
+  /*
+     The value of the selected option contains custid of a customer. This custid is sent to view for fetching bill details.
+     The view returns a jinja template with bill details represented in tabular form. It also returns unadjusted amounts.
+   */
   $(document).off('change', '#customerselect').on('change', '#customerselect', function(event) {
     event.preventDefault();
     $.ajax(
@@ -41,17 +52,19 @@ $(document).ready(function() {
         success: function(resp)
         {
 	  sessionStorage.customeramount =  0.00;
-          $("#billwisediv").html(resp);
-	  $(".panel-footer").show();
-	  $('#supplierselect option[value=""]').prop("selected", true);
-	  $("#cslisttitle").show();
-	  $("#csname").html($("#customerselect option:selected").text());
+          $("#billwisediv").html(resp);  //Here, resp contains jinja template with bill data. It is loaded into a div.
+	  $(".panel-footer").show();  //Footer buttons are displayed.
+	  $('#supplierselect option[value=""]').prop("selected", true);  //If a customer is selected then the list of suppliers is reset.
+	  $("#cslisttitle").show();  //Displays a the title of the table.
+	  $("#csname").html($("#customerselect option:selected").text());  //Loads customer name into a span to be displayed next to the title.
         }
       }
     );
   });
 
+  //Actions that occur when key is pressed down while at list of customers.  
   $(document).off('keydown', '#customerselect').on('keydown', '#customerselect', function(event) {
+    //When enter key is pressed focus shifts to first of amount paid fields. If no field is visible focus shifts to buttons at the footer.
     if (event.which == 13) {
       event.preventDefault();
       if ($(".amountpaid").length > 0) {
@@ -62,7 +75,7 @@ $(document).ready(function() {
       }
     }
   });
-  
+  //This is similar to the change event of list of customers.
   $(document).off('change', '#supplierselect').on('change', '#supplierselect', function(event) {
     event.preventDefault();
     $.ajax(
@@ -82,14 +95,14 @@ $(document).ready(function() {
 	  sessionStorage.customeramount =  0.00;
           $("#billwisediv").html(resp);
 	  $(".panel-footer").show();
-	  $('#customerselect option[value=""]').prop("selected", true);
+	  $('#customerselect option[value=""]').prop("selected", true);  //Here list of suppliers is reset when a supplier is selected.
 	  $("#cslisttitle").show();
 	  $("#csname").html($("#supplierselect option:selected").text());
         }
       }
     );
   });
-
+  //Key events for list of suppliers which is similar to that of list of customers.
   $(document).off('keydown', '#supplierselect').on('keydown', '#supplierselect', function(event) {
     if (event.which == 13) {
       event.preventDefault();
