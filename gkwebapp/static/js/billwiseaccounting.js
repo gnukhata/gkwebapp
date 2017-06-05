0289b0e2bc0e59602cbf04034d40e26c45f7f108
abhi1tb <abhijithb21@openmailbox.org>
2017-06-05 20:28:26 +0530

    Changed the way nodata found error is fixed

--- gkwebapp/static/js/billwiseaccounting.js
+++ gkwebapp/static/js/billwiseaccounting.js
@@ -319,7 +319,7 @@ $(document).ready(function() {
 	});
 	return false;
       }
-      if ($("#latable tbody tr:eq("+i+")").data("invid") != "") {
+      if ($("#latable tbody tr:eq("+i+") td:eq(4) input").length != 0) {
 	//Creating a dictionary and appending to the list.
 	var amountpaid = parseFloat($("#latable tbody tr:eq("+i+") td:eq(4) input").val());
 	var invid = parseInt($("#latable tbody tr:eq("+i+")").data("invid"));
