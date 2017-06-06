f3e77d18b3779af83014b2edc358a4dc2c35dc7f
abhi1tb <abhijithb21@openmailbox.org>
2017-06-06 18:29:35 +0530

    Changed Amount Paid to Amount Adjusted.

--- gkwebapp/static/js/addvoucher.js
+++ gkwebapp/static/js/addvoucher.js
@@ -1271,7 +1271,7 @@ $("#invsel").keyup(function(event) {
 		customercode = accountdetails["accountcode"];
 		numberofcustomers = numberofcustomers + 1;
 		sessionStorage.customeramount = $("#vtable tbody tr:eq("+curindex+") td:eq(4) input").val(); //Credit amount is saved in session storage.
-		sessionStorage.amounttitle = "Amount to be paid: ";
+		sessionStorage.amounttitle = "Amount Received: ";
 	      }
 	    }
 	  });
@@ -1299,7 +1299,7 @@ $("#invsel").keyup(function(event) {
 		customercode = accountdetails["accountcode"];
 		numberofcustomers = numberofcustomers + 1;
 		sessionStorage.customeramount = $("#vtable tbody tr:eq("+curindex+") td:eq(3) input").val();
-		sessionStorage.amounttitle = "Amount to be paid: ";
+		sessionStorage.amounttitle = "Amount Paid: ";
 	      }
 	    }
 	  });
