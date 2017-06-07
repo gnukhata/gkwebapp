cae8ad8103feb963a46eb2b6bd0f0e8ac9a7c4ea
Bhavesh <bbhavesh07@gmail.com>
2017-06-08 00:56:10 +0530

    view another register report button handled

---
+++ gkwebapp/static/js/registerreport.js
@@ -0,0 +1,59 @@
+/*
+Copyright (C) 2013, 2014, 2015, 2016 Digital Freedom Foundation
+  This file is part of GNUKhata:A modular,robust and Free Accounting System.
+
+  GNUKhata is Free Software; you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation; either version 3 of
+  the License, or (at your option) any later version.
+
+  GNUKhata is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public
+  License along with GNUKhata (COPYING); if not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA  02110-1301  USA59 Temple Place, Suite 330,
+
+
+Contributors:
+"Bhavesh Bawadhane" <bbhavesh07@gmail.com>
+*/
+// This script is for log report.
+$(document).ready(function() {
+  $('.modal-backdrop').remove();
+  $("#msspinmodal").modal("hide");
+  $(".fixed-table-loading").remove();
+  $('#rclearfields').click(function(){
+    $(this).siblings(".bootstrap-table").find(".form-control").val("");
+  });
+  $("#anotherregister").click(function(event) {
+    $("#showviewregister").click();
+  });
+/*
+  $('#exporttospreadsheetregister').click(function (e) {
+    $.ajax(
+      {
+        type: "POST",
+        url: "/log?action=printableshowlogreport",
+        global: false,
+        async: false,
+        datatype: "text/html",
+        data: {"typeflag":$('#logof').val(),"userid":$('#userid').val(),"username":$('#username').val(), "calculatefrom":$('#calculatefrom').val(),"calculateto":$('#calculateto').val()},
+        beforeSend: function(xhr)
+        {
+          xhr.setRequestHeader('gktoken',sessionStorage.gktoken );
+        },
+      })
+        .done(function(resp)
+        {
+          $("#info").html(resp);
+          console.log($('#userid').val()+$('#username').val());
+        }
+      );
+
+    });
+*/
+});
