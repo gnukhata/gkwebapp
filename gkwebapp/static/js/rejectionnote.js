9d0850ed6b830321c3d1410bdb7aa9274d23136c
Bhavesh <bbhavesh07@gmail.com>
2017-06-13 14:21:17 +0530

    ajax call to show view template also default form is set to rejection in

--- gkwebapp/static/js/rejectionnote.js
+++ gkwebapp/static/js/rejectionnote.js
@@ -40,6 +40,26 @@ $(document).ready(function() {
     }
   );
   });
+  $("#rejectionnote_view").click(function() {
+    $.ajax(
+    {
 
+    type: "POST",
+    url: "/rejectionnote?action=showview",
+    global: false,
+    async: false,
+    datatype: "text/html",
+    beforeSend: function(xhr)
+      {
+        xhr.setRequestHeader('gktoken',sessionStorage.gktoken );
+      },
+    success: function(resp)
+    {
+      $("#rejectionnote_div").html(resp);
+    }
+    }
+  );
+  });
+  $("#rejectionnote_in").click();
 
 });
