3f132cd4d370c0d2ed2d9ac80bb4cd4237ee3ea7
Bhavesh <bbhavesh07@gmail.com>
2017-06-13 14:20:38 +0530

    ajax call to show rejection out template

--- gkwebapp/static/js/rejectionnote.js
+++ gkwebapp/static/js/rejectionnote.js
@@ -20,6 +20,26 @@ $(document).ready(function() {
     }
   );
   });
+  $("#rejectionnote_out").click(function() {
+    $.ajax(
+    {
+
+    type: "POST",
+    url: "/rejectionnote?action=showadd&status=out",
+    global: false,
+    async: false,
+    datatype: "text/html",
+    beforeSend: function(xhr)
+      {
+        xhr.setRequestHeader('gktoken',sessionStorage.gktoken );
+      },
+    success: function(resp)
+    {
+      $("#rejectionnote_div").html(resp);
+    }
+    }
+  );
+  });
 
 
 });
