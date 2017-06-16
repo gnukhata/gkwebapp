008b84fa9b8436f1b99341bf5ac630a01f5d462d
Bhavesh <bbhavesh07@gmail.com>
2017-06-17 00:45:40 +0530

    else conditions added

--- gkwebapp/static/js/viewrejectionnote.js
+++ gkwebapp/static/js/viewrejectionnote.js
@@ -18,12 +18,16 @@ $(document).ready(function() {
       $("#rejectionnote_noteno").val(resp.gkresult.rnno);
       if(resp.gkresult.invid)
         $("#rejectionnote_invoice").val(resp.gkresult.invno + ", " + resp.gkresult.invdate);
+      else
+        $("#rejectionnote_invoice").val("None");
       if(resp.gkresult.dcid)
         $("#rejectionnote_deliverynote").val(resp.gkresult.dcno + ", " + resp.gkresult.dcdate);
-      var dcdatearray = resp.gkresult.rndate.split(/\s*\-\s*/g);
-      $("#rejectionnote_date").val(dcdatearray[0]);
-      $("#rejectionnote_month").val(dcdatearray[1]);
-      $("#rejectionnote_year").val(dcdatearray[2]);
+      else
+        $("#rejectionnote_deliverynote").val("None");
+      var rndatearray = resp.gkresult.rndate.split(/\s*\-\s*/g);
+      $("#rejectionnote_date").val(rndatearray[0]);
+      $("#rejectionnote_month").val(rndatearray[1]);
+      $("#rejectionnote_year").val(rndatearray[2]);
       if (resp.gkresult.inout==9) {
         $("#ctinlabel").show();
         $("#clabel").show();
@@ -47,13 +51,24 @@ $(document).ready(function() {
       if(resp.gkresult.hasOwnProperty("dcno")){
           $("#rejectionnote_consignment").val(resp.gkresult.transactiontype);
       }
+      else{
+        $("#rejectionnote_consignment").val("None");
+      }
       if(resp.gkresult.hasOwnProperty("custname")){
         $("#rejectionnote_customer").val(resp.gkresult.custname);
         $("#rejectionnote_customeraddr").val(resp.gkresult.custaddr);
         $("#rejectionnote_customertin").val(resp.gkresult.custtin);
       }
+      else{
+        $("#rejectionnote_customer").val("None");
+        $("#rejectionnote_customeraddr").val("None");
+        $("#rejectionnote_customertin").val("None");
+      }
       if(resp.gkresult.hasOwnProperty("goname")){
         $('#rejectionnote_godown').val(resp.gkresult.goname + "(" + resp.gkresult.gostate + ")");
+      }
+      else {
+        $("#rejectionnote_godown").val("None");
       }
       $('#rejectionnote_product_table tbody').empty();
       $.each(resp.gkresult.rejected, function(key, value) {
