889851252dedbadea89fb1ebe3151d3646951beb
reshma1 <bhatawadekar1reshma@gmail.com>
2017-07-31 18:31:25 +0530

    now we can see cust name in invoice field with inv no and invoice date

--- gkwebapp/views/editvoucher.py
+++ gkwebapp/views/editvoucher.py
@@ -124,7 +124,7 @@ def viewvoucher(request):
 	gkdata = {"code":int(vcode)}
 	result = requests.get("http://127.0.0.1:6543/transaction?code=%d"%(int(request.params["id"])),headers=header)
 	vc=result.json()["gkresult"]
-	print vc
+	
 	type = vc["vouchertype"]
 	projects = requests.get("http://127.0.0.1:6543/projects", headers=header)
 	if type=="contra" or type=="journal":
@@ -150,7 +150,7 @@ def viewvoucher(request):
 
 
 	if result.json()["gkstatus"]==0:
-		
+
 		if vc["invid"]!=None:
 			viewinvdata = requests.get("http://127.0.0.1:6543/invoice?inv=single&invid=%d"%(vc["invid"]), headers=header)
 			vvi=viewinvdata.json()["gkresult"]
