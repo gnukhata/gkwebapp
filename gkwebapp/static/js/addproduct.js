cc7f2e017de1214322ab6c66af858906d8e005f9
robaraskar <robaraskar@gmail.com>
2017-08-04 14:00:46 +0530

    addproduct is working flawlessly

--- gkwebapp/static/js/addproduct.js
+++ gkwebapp/static/js/addproduct.js
@@ -860,103 +860,68 @@ $(document).off("keydown",".tax_rate").on("keydown",".tax_rate",function(event)
   if (event.which==13) {
     $('#product_tax_table tbody tr:eq('+curindex1+') td:eq(2) input').val(parseFloat($('#product_tax_table tbody tr:eq('+curindex1+') td:eq(2) input').val()).toFixed(2));
     event.preventDefault();
-    if ($('#product_tax_table tbody tr:eq('+curindex1+') td:eq(1) select option:selected').attr("stateid") < 1 && selectedtaxname == "VAT") {
-      $("#tax_state-blank-alert").alert();
-      $("#tax_state-blank-alert").fadeTo(2250, 500).slideUp(500, function(){
-        $("#tax_state-blank-alert").hide();
-      });
-      return false;
-    }
-    if (curindex1 != ($("#product_tax_table tbody tr").length-1)) {
-      $('#product_tax_table tbody tr:eq('+nextindex1+') td:eq(0) select').focus().select();
-    }
-    else {
-      if ($('#product_tax_table tbody tr:eq('+curindex1+') td:eq(0) select').val()=="") {
-        $("#tax-name-blank-alert").alert();
-        $("#tax-name-blank-alert").fadeTo(2250, 500).slideUp(500, function(){
-          $("#tax-name-blank-alert").hide();
-        });
-        $('#product_tax_table tbody tr:eq('+curindex1+') td:eq(0) select').focus();
-        return false;
+    if ($('#product_tax_table tbody tr:eq('+curindex1+') td:eq(0) select option:selected').val()=="IGST") {
+      console.log("yes yes");
+      if($("#additem option:selected").val()=='19'){
+      $("#apsubmit").focus();
       }
-      if ($('#product_tax_table tbody tr:eq('+curindex1+') td:eq(2) input').val()=="") {
-        $("#tax-rate-blank-alert").alert();
-        $("#tax-rate-blank-alert").fadeTo(2250, 500).slideUp(500, function(){
-          $("#tax-rate-blank-alert").hide();
-        });
-        $('#product_tax_table tbody tr:eq('+curindex1+') td:eq(2) input').focus();
-        return false;
-      }
+      else{
 
-      var taxhtml = $('#product_tax_table tbody tr:first').html();
-      
-      $('#product_tax_table tbody').append('<tr>'+ taxhtml + '</tr>');
-      /*$('#product_tax_table tbody').append('<tr>'+
-      '<td class="col-xs-4">'+
-      '<select class="form-control input-sm tax_name product_new_name">'+
-      '<option value="" selected>Select Tax</option>'+
-      '<option value="VAT">VAT</option>'+
-      '<option value="CVAT">CVAT</option>'+
-      '<option value="IGST">IGST</option>'+
-      '</select>'+
-      '</td>'+
-      '<td class="col-xs-4">'+
-      '<select class="form-control input-sm tax_state product_new_state" >'+
-      '<option value="">None</option>'+
-      '<option value="Andaman and Nicobar Islands" stateid="35">Andaman and Nicobar Islands</option>'+
-      '<option value="Andhra Pradesh" stateid="28">Andhra Pradesh</option>'+
-      '<option value="Andhra Pradesh New" stateid="37">Andhra Pradesh (New)</option>'+
-      '<option value="Arunachal Pradesh" stateid="12">Arunachal Pradesh</option>'+
-      '<option value="Assam" stateid="18">Assam</option>'+
-      '<option value="Bihar" stateid="10">Bihar</option>'+
-      '<option value="Chandigarh" stateid="4">Chandigarh</option>'+
-      '<option value="Chhattisgarh" stateid="22">Chhattisgarh</option>'+
-      '<option value="Dadra and Nagar Haveli" stateid="26">Dadra and Nagar Haveli</option>'+
-      '<option value="Daman and Diu" stateid="25">Daman and Diu</option>'+
-      '<option value="Delhi" stateid="7">Delhi</option>'+
-      '<option value="Goa" stateid="20">Goa</option>'+
-      '<option value="Gujarat" stateid="29">Gujarat</option>'+
-      '<option value="Haryana" stateid="6">Haryana</option>'+
-      '<option value="Himachal Pradesh" stateid="2">Himachal Pradesh</option>'+
-      '<option value="Jammu and Kashmir" stateid="1">Jammu and Kashmir</option>'+
-      '<option value="Jharkhand" stateid="20">Jharkhand</option>'+
-      '<option value="Karnataka" stateid="29">Karnataka</option>'+
-      '<option value="Kerala" stateid="32">Kerala</option>'+
-      '<option value="Lakshadweep" stateid="31">Lakshadweep</option>'+
-      '<option value="Madhya Pradesh" stateid="23">Madhya Pradesh</option>'+
-      '<option value="Maharashtra" stateid="27">Maharashtra</option>'+
-      '<option value="Manipur" stateid="14">Manipur</option>'+
-      '<option value="Meghalaya" stateid="17">Meghalaya</option>'+
-      '<option value="Mizoram" stateid="15">Mizoram</option>'+
-      '<option value="Nagaland" stateid="13">Nagaland</option>'+
-      '<option value="Odisha" stateid="21">Odisha</option>'+
-      '<option value="Pondicherry" stateid="34">Pondicherry</option>'+
-      '<option value="Punjab" stateid="3">Punjab</option>'+
-      '<option value="Rajasthan" stateid="8">Rajasthan</option>'+
-      '<option value="Sikkim" stateid="11">Sikkim</option>'+
-      '<option value="Tamil Nadu" stateid="33">Tamil Nadu</option>'+
-      '<option value="Telangana" stateid="36">Telangana</option>'+
-      '<option value="Tripura" stateid="16">Tripura</option>'+
-      '<option value="Uttar Pradesh" stateid="9">Uttar Pradesh</option>'+
-      '<option value="Uttarakhand" stateid="5">Uttarakhand</option>'+
-      '<option value="West Bengal" stateid="19">West Bengal</option>'+
-      '</select>'+
-      '</td>'+
-      '<td class="col-xs-3">'+
-      '<input class="form-control input-sm tax_rate text-right product_new_rate 3pe"  placeholder="Rate">'+
-      '</td>'+
-      '<td class="col-xs-1">'+
-      '<a href="#" class="tax_del"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a>'+
-      '</td>'+
-      '</tr>');*/
-      console.log("rooo"+taxhtml);
-      $(".tax_rate").numeric();
-      if (selectedtaxname == "CVAT" || selectedtaxname == "IGST" ) {
-        $('#product_tax_table tbody tr:eq('+nextindex1+') td:eq(0) select option[value='+selectedtaxname+']').prop('hidden', true).prop('disabled', true);
+        if ($("#godownpresence").val()==0) {
+          $("#openingstock").focus().select();
+        }
+        if ($("#godownpresence").val()==1)
+        {
+          $("#godownflag").focus().select();
+        }
+        if(sessionStorage.invflag==0){
+          $("#apsubmit").focus();
+        }
       }
-      $('#product_tax_table tbody tr:eq('+nextindex1+') td:eq(0) select').focus().select();
     }
-  }
+    else{
+
+      if ($('#product_tax_table tbody tr:eq('+curindex1+') td:eq(1) select option:selected').attr("stateid") < 1 && selectedtaxname == "VAT") {
+        $("#tax_state-blank-alert").alert();
+        $("#tax_state-blank-alert").fadeTo(2250, 500).slideUp(500, function(){
+          $("#tax_state-blank-alert").hide();
+        });
+        return false;
+      }
+      if (curindex1 != ($("#product_tax_table tbody tr").length-1)) {
+        $('#product_tax_table tbody tr:eq('+nextindex1+') td:eq(0) select').focus().select();
+      }
+      else {
+        if ($('#product_tax_table tbody tr:eq('+curindex1+') td:eq(0) select').val()=="") {
+          $("#tax-name-blank-alert").alert();
+          $("#tax-name-blank-alert").fadeTo(2250, 500).slideUp(500, function(){
+            $("#tax-name-blank-alert").hide();
+          });
+          $('#product_tax_table tbody tr:eq('+curindex1+') td:eq(0) select').focus();
+          return false;
+        }
+        if ($('#product_tax_table tbody tr:eq('+curindex1+') td:eq(2) input').val()=="") {
+          $("#tax-rate-blank-alert").alert();
+          $("#tax-rate-blank-alert").fadeTo(2250, 500).slideUp(500, function(){
+            $("#tax-rate-blank-alert").hide();
+          });
+          $('#product_tax_table tbody tr:eq('+curindex1+') td:eq(2) input').focus();
+          return false;
+        }
+
+        var taxhtml = $('#product_tax_table tbody tr:first').html();
+
+        $('#product_tax_table tbody').append('<tr>'+ taxhtml + '</tr>');
+        $('#product_tax_table tbody tr:last td:last').append('<a href="#" class="tax_del"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a>');
+
+        $(".tax_rate").numeric();
+        if (selectedtaxname == "CVAT" || selectedtaxname == "IGST" ) {
+          $('#product_tax_table tbody tr:eq('+nextindex1+') td:eq(0) select option[value='+selectedtaxname+']').prop('hidden', true).prop('disabled', true);
+        }
+        $('#product_tax_table tbody tr:eq('+nextindex1+') td:eq(0) select').focus().select();
+      }
+    }
+  }//13 event ends here
   else if(event.which==190 && event.shiftKey)
   {
     event.preventDefault();
--- gkwebapp/views/product.py
+++ gkwebapp/views/product.py
@@ -43,7 +43,6 @@ def showproducttab(request):
 	header={"gktoken":request.headers["gktoken"]}
 	result = requests.get("http://127.0.0.1:6543/products",headers=header)
 	resultgstvat = requests.get("http://127.0.0.1:6543/products?tax=vatorgst",headers=header)
-	print resultgstvat.json()["gkresult"]
 	return{"numberofproducts":len(result.json()["gkresult"]),"gkstatus":result.json()["gkstatus"],"vatorgstflag":resultgstvat.json()["gkresult"]}
 
 @view_config(route_name="product",request_param="type=addtab", renderer="gkwebapp:templates/addproduct.jinja2")
@@ -53,7 +52,6 @@ def addproducttab(request):
 	result1 = requests.get("http://127.0.0.1:6543/unitofmeasurement?qty=all", headers=header)
 	result2 = requests.get("http://127.0.0.1:6543/godown", headers=header)
 	resultgstvat = requests.get("http://127.0.0.1:6543/products?tax=vatorgst",headers=header)
-	print resultgstvat.json()["gkresult"]
 	return{"gkresult":{"category":result.json()["gkresult"],"uom":result1.json()["gkresult"]},"godown":result2.json()["gkresult"],"gkstatus":result.json()["gkstatus"],"vatorgstflag":resultgstvat.json()["gkresult"]}
 
 @view_config(route_name="product",request_param="type=specs", renderer="gkwebapp:templates/addproductspecs.jinja2")
@@ -259,8 +257,8 @@ def deleteproduct(request):
 def editproducttab(request):
 	header={"gktoken":request.headers["gktoken"]}
 	result = requests.get("http://127.0.0.1:6543/products",headers=header)
-
-	return{"gkresult":result.json()["gkresult"],"gkstatus":result.json()["gkstatus"]}
+	resultgstvat = requests.get("http://127.0.0.1:6543/products?tax=vatorgst",headers=header)
+	return{"gkresult":result.json()["gkresult"],"gkstatus":result.json()["gkstatus"],"vatorgstflag":resultgstvat.json()["gkresult"]}
 
 @view_config(route_name="product",request_param="type=details", renderer="gkwebapp:templates/editproductspecs.jinja2")
 def productdetails(request):
