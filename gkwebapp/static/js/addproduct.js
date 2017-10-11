e73f591aa240d0b29af0267b267b31fa8409bddb
abhi1tb <abhijithb21@disroot.org>
2017-10-11 13:31:28 +0530

    Added back license

--- gkwebapp/static/js/addproduct.js
+++ gkwebapp/static/js/addproduct.js
@@ -1,4 +1,35 @@
+/*
+  Copyright (C) 2013, 2014, 2015, 2016, 2017 Digital Freedom Foundation
+  This file is part of GNUKhata:A modular,robust and Free Accounting System.
 
+  GNUKhata is Free Software; you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation; either version 3 of
+  the License, or (at your option) any later version.
+
+  GNUKhata is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public
+  License along with GNUKhata (COPYING); if not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA  02110-1301  USA59 Temple Place, Suite 330,
+
+
+  Contributors:
+  "Abhijith Balan" <abhijithb21@openmailbox.org>
+  "Bhavesh Bawadhane" <bbhavesh07@gmail.com>
+  "Ishan Masdekar" <imasdekar@dff.org.in>
+  "Mohd. Talha Pawaty" <mtalha456@gmail.com>
+  "Navin Karkera" <navin@openmailbox.org>
+  "Ravishankar Purne" <ravismail96@gmail.com>
+  "Reshma Bhatawadekar" <bhatawadekar1reshma@gmail.com>
+  "Rohini Baraskar" <robaraskar@gmail.com>
+  "Sachin Patil" <sachin619patil@rediffmail.com>
+
+*/
 
 
 $(document).ready(function() {
