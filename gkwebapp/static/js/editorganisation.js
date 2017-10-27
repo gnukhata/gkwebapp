a4b120d6bc089540760392146a30170db6957a31
Prajkta_Patkar <prajkta.patkar007@gmail.com>
2017-10-27 17:39:42 +0530

    states input format chnaged to selection box. also if state is saved it will be shown

--- gkwebapp/static/js/editorganisation.js
+++ gkwebapp/static/js/editorganisation.js
@@ -5,7 +5,7 @@ Copyright (C) 2013, 2014, 2015, 2016 Digital Freedom Foundation
   GNUKhata is Free Software; you can redistribute it and/or modify
   it under the terms of the GNU Affero General Public License as
   published by the Free Software Foundation; either version 3 of
-  the License, or (at your option) any later version.and old.stockflag = 's'
+  the License, or (at your option) any later version.
 
   GNUKhata is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -29,7 +29,7 @@ $(document).ready(function(){
   $("#msspinmodal").modal("hide");
   $(".regdate").autotab('number');
   $(".fcradate").autotab('number');
-
+    $("#orgstate").val($("#state").val());
   $(".regdate").numeric({negative: false});
   $(".fcradate").numeric({negative: false});
 
