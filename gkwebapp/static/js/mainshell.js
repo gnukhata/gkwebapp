00cdae8bac4aa2503210452d241b34659e96777f
reshma1 <bhatawadekar1reshma@gmail.com>
2017-10-30 17:42:00 +0530

    Master, Inventory and Administration menus are hidden for Internal Auditor User Role.

--- gkwebapp/static/js/mainshell.js
+++ gkwebapp/static/js/mainshell.js
@@ -515,8 +515,13 @@ var userrole1;
        $("#orgpref").remove();
      }
      if(resp["gkresult"]["userrole"]==2) {
-       $("#showviewlog").remove();
-       $("#orgpref").remove();
+	 $("#showviewlog").remove();
+	 $("#orgpref").remove();
+	 $(".mastermenu").remove();
+	 $(".inventorymenu").remove();
+	 $(".administrationmenu").remove();
+	 $(".intauditor").remove();
+	 $("#fevoucher").text("Find Voucher");
      }
      if (resp["gkresult"]["booksclosedflag"]==1) {
        $(".closebooks").remove();
