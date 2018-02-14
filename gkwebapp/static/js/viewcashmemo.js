5c302c2cd17085ed14f667d31d7cae23d2025889
Nitesh <nitesh.chaughule5@gmail.com>
2018-02-14 15:07:21 +0530

    In view Cash Memo, adding radio button for selecting particular cash memo (i.e recorded or created). For recorded cash memo "print" button is disabled.

--- gkwebapp/static/js/viewcashmemo.js
+++ gkwebapp/static/js/viewcashmemo.js
@@ -1,15 +1,31 @@
 $(document).ready(function() {
     $('.modal-backdrop').remove();
     $('.invoicedate').autotab('number');
-    $("#invoice_all_no").focus();
+    //$("#invoice_all_no").focus();
+    $("#all").focus();
     $("#cashmemo_print").hide();
+
+    $(document).off('keydown', '.iib').on('keydown', '.iib', function(event) {
+	if (event.which == 13) {
+	    event.preventDefault();
+	    if($("#all").is(":checked")){
+		$("#invoice_all_no").focus();
+	    }
+	    else if ($("#reccashmemo").is(":checked")) {
+		$("#invoice_all_no").focus();
+	    }
+	    else if ($("#createcashmemo").is(":checked")) {
+		$("#invoice_all_no").focus();
+	    }
+	}
+    });
+
+    
   $("#invoice_all_no").change(function(event) {
         /*
         Getting complete information for the selected cash memo using its id.
         Display details in the corresponding fields.
 */
-
-
         var invid = $("#invoice_all_no option:selected").val();
         console.log("inv selected"+invid);
         if (invid != "") {
@@ -30,7 +46,9 @@ $(document).ready(function() {
                       $("#viewcashmemodiv").html(resp);
                     console.log("success");
 		    $("#viewinvfooter").show();
-                    $("#cashmemo_print").show();
+		    if($("#invoice_all_no option:selected").attr("inout") == 15){
+			$("#cashmemo_print").show();
+		    }else{ $("#cashmemo_print").hide(); }
                 })
                 .fail(function() {
                     console.log("error");
