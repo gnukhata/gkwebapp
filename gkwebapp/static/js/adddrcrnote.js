6ffbb3802f338bf90ceab9c3acee1e99866b0309
RESHMA <bhatawadekar1reshma@gmail.com>
2018-03-06 19:14:18 +0530

    1. changes in css as per requirement 2. auto tab on date. 3. tables are added for gst and vat(alignment problem)

--- gkwebapp/static/js/adddrcrnote.js
+++ gkwebapp/static/js/adddrcrnote.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     console.log("Ready");
     $("#drcrnote_invoice").focus();
+    $('.drcrnotedate').autotab('number');  //Focus shifts from fields among date fields.
 });
 
 
