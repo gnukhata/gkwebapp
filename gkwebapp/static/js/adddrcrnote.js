496d779c538b07630bc5cc092e04c7ea1a4bfe1f
RESHMA <bhatawadekar1reshma@gmail.com>
2018-03-08 11:30:50 +0530

    code for adding row in table.

--- gkwebapp/static/js/adddrcrnote.js
+++ gkwebapp/static/js/adddrcrnote.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
     console.log("Ready");
     $("#drcrnote_invoice").focus();
     $('.drcrnotedate').autotab('number');  //Focus shifts from fields among date fields.
+    var financialstart = Date.parseExact(sessionStorage.yyyymmddyear1, "yyyy-MM-dd");  //Start of financial year is saved in a variable.
+    var financialend = Date.parseExact(sessionStorage.yyyymmddyear2, "yyyy-MM-dd");  //End of financial year is saved in a variable.
+    
+    //Whenever a new row in a table is to be added html for a row is to be appended to table body. Such html is stored in variables.
+    var gsthtml = $('#drcrnote_product_table_gst tbody tr:first').html();  //HTML for GST Product Table row.
+    var totaltablehtml = $("#drcrnote_product_table_total tbody tr:first").html(); //HTML for table displaying totals in GST Product Table.
+    var vathtml = $('#drcrnote_table_vat tbody tr:first').html();  //HTML for VAT Product Table row.
+    
     $("#drcrnote_invoice").change(function(event){
 	$.ajax({
 		url: '/invoice?action=getinvdetails',
@@ -14,7 +22,8 @@ $(document).ready(function() {
 		   
 		}
 	})
-	       .done(function(resp) {
+	    .done(function(resp) {
+		console.log("success");
 		if (resp["gkstatus"] == 0) {
 		    console.log(resp);
 		    //Load invoice data
@@ -23,6 +32,33 @@ $(document).ready(function() {
 		    $("#gstin").text(resp.invoicedata.custSupDetails["custgstin"]);
 		    $("#tin").text(resp.invoicedata.custSupDetails["custtin"]);
 		    $("#drcrnote_custsuppaddr").text(resp.invoicedata.custSupDetails["custaddr"]);
+		    $("#taxapplicable").text(resp.invoicedata.taxname);
+		    $.each(resp.items, function(key, value) {
+			$('#drcrnote_table_vat tbody').append('<tr>' + vathtml + '</tr>');
+			$('#drcrnote_table_vat tbody tr:last td:last').append('<a href="#" class="product_del"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a>');
+			$('#drcrnote_table_vat tbody tr:last td:first select').val(key).prop("disabled", true);
+			$('#drcrnote_table_vat tbody tr:last td:eq(1) input').val(parseFloat(value.qty).toFixed(2));
+			$('#drcrnote_table_vat tbody tr:last td:eq(1) input').val(parseFloat(value.qty).toFixed(2)).attr("data", parseFloat(value.qty).toFixed(2));
+			$('#drcrnote_table_vat tbody tr:last td:eq(1) span').text(value.unitname);
+			$('#drcrnote_table_vat tbody tr:last td:eq(2) span').text(value.unitname);
+		    });
+		    $("#drcrnote_table_vat tbody tr:first td:eq(9)").empty();
+		    var productcode;
+		    $.each(resp.items, function(key, value) {
+			$('#drcrnote_product_table_gst tbody').append('<tr>'+ gsthtml + '</tr>');
+			$('#drcrnote_product_table_gst tbody tr:last td:first select').val(key).prop("disabled", true);
+			$('#drcrnote_product_table_gst tbody tr:last td:eq(1) label').html(value.gscode);
+			$('#drcrnote_product_table_gst tbody tr:last td:eq(2) input').val(parseFloat(value.qty).toFixed(2));
+			$('#drcrnote_product_table_gst tbody tr:last td:eq(2) input').val(parseFloat(value.qty).toFixed(2)).attr("data", parseFloat(value.qty).toFixed(2));
+			$('#drcrnote_product_table_gst tbody tr:last td:eq(2) span').text(value.unitname);
+			$('#drcrnote_product_table_gst tbody tr:last td:eq(3) span').text(value.unitname);
+			$('.drcrnote_product_quantity_gst').numeric({ negative: false });
+			$('.drcrnote_product_per_price_gst').numeric({ negative: false });
+			$("#drcrnote_product_table_total tbody").append('<tr>'+ totaltablehtml + '</tr>');
+			$('#drcrnote_product_table_total tbody tr:last td:last').append('<a href="#" class="product_del"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a>');
+		    });
+		    $("#invoice_product_table_total tbody tr:first td:last").empty();
+		    $(".product_name_gst, .product_name_vat, #invoicestate").change();
 		    if(resp.invoicedata.inoutflag == "15") {
 			console.log("Source State");
 			console.log(resp.invoicedata.sourcestate);
