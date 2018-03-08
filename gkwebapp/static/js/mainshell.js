2a3ff9807892557b3075a8ec387c3f33d7d3fc50
Prajkta Patkar <prajkta@disroot.org>
2018-03-08 14:54:56 +0530

    added new file and changed copyright

--- gkwebapp/static/js/mainshell.js
+++ gkwebapp/static/js/mainshell.js
@@ -1,23 +1,23 @@
 /*
-   Copyright (C) 2013, 2014, 2015, 2016 Digital Freedom Foundation
-   Copyright (C) 2017, 2018 Digital Freedom Foundation & Accion Labs Pvt. Ltd.
+Copyright (C) 2013, 2014, 2015, 2016 Digital Freedom Foundation
+Copyright (C) 2017, 2018 Digital Freedom Foundation & Accion Labs Pvt. Ltd.
 
-   This file is part of GNUKhata:A modular,robust and Free Accounting System.
+  This file is part of GNUKhata:A modular,robust and Free Accounting System.
 
-   GNUKhata is Free Software; you can redistribute it and/or modify
-   it under the terms of the GNU Affero General Public License as
-   published by the Free Software Foundation; either version 3 of
-   the License, or (at your option) any later version.
+  GNUKhata is Free Software; you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation; either version 3 of
+  the License, or (at your option) any later version.
 
-   GNUKhata is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Affero General Public License for more details.
+  GNUKhata is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
 
-   You should have received a copy of the GNU Affero General Public
-   License along with GNUKhata (COPYING); if not, write to the
-   Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA  02110-1301  USA59 Temple Place, Suite 330,
+  You should have received a copy of the GNU Affero General Public
+  License along with GNUKhata (COPYING); if not, write to the
+  Free Software Foundation, Inc.,51 Franklin Street, 
+  Fifth Floor, Boston, MA 02110, United States
 
 
    Contributors:
@@ -30,7 +30,7 @@
    "Parabjyot Singh" <parabjyot1996@gmail.com>
    "Rahul Chaurasiya" <crahul4133@gmail.com>
    "Mohd. Talha Pawaty" <mtalha456@gmail.com>
-   "prajkta Patkar" <prajakta@dff.org.in>
+   "prajkta Patkar" <prajkta@riseup.net>
    "Reshma Bhatawadekar" <reshma@dff.org.in>
  */
 // This script is for the mainshell page and loads when the main page of GNUKhata is loaded.
