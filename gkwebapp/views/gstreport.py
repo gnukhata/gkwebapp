9767a4f6fd41ec8b5f6e1e352b7677da49825ec8
Prajkta Patkar <prajkta@disroot.org>
2018-05-04 19:35:08 +0530

    code for loop started and now it will be optimized

--- gkwebapp/views/gstreport.py
+++ gkwebapp/views/gstreport.py
@@ -70,31 +70,74 @@ def gstsummspreadsheet(request):
         result = requests.get("http://127.0.0.1:6543/report?type=GSTCalc",data =json.dumps(gkdata), headers=header)
         
         data = result.json()["gkresult"]
-        data["lenSGSTin"] = len(result.json()["gkresult"]["sgstin"])
+        '''
+        lenSGSTin = len(result.json()["gkresult"]["sgstin"])
         data["lenSGSTout"] = len(result.json()["gkresult"]["sgstout"])
         data["lenCGSTin"] =  len(result.json()["gkresult"]["cgstin"])
         data["lenCGSTout"] = len(result.json()["gkresult"]["cgstout"])
         data["lenIGSTin"] =  len(result.json()["gkresult"]["igstin"])
         data["lenIGSTout"] =  len(result.json()["gkresult"]["igstout"])
         data["lenCESSin"] =  len(result.json()["gkresult"]["cessin"])
-        data["lenCESSout"] = len(result.json()["gkresult"]["cessout"])
+        data["lenCESSout"] = len(result.json()["gkresult"]["cessout"])'''
         gstsmwb = openpyxl.Workbook()
         sheet = gstsmwb.active
         sheet.title= "GST Summary "
-        sheet.merge_cells('A1:G2')
+        sheet.merge_cells('A1:E2')
         sheet.column_dimensions['A'].width = 8
         sheet['A1'].font = Font(name='Liberation Serif',size='16',bold=True)
         sheet['A1'].alignment = Alignment(horizontal = 'center', vertical='center')
         sheet['A1'] = str(request.params["orgname"])
-        sheet.merge_cells('A3:G3')
+        sheet.merge_cells('A3:E3')
         sheet['A3'].font = Font(name='Liberation Serif',size='14',bold=True)
         sheet['A3'].alignment = Alignment(horizontal = 'center', vertical='center')
         sheet['A3'] = 'Statement of GST Calculation '
         sheet.merge_cells('A4:G4')
-        sheet['A4'].font = Font(name='Liberation Serif',size='10',bold=True)
+        sheet['A4'].font = Font(name='Liberation Serif',size='12',italic=True)
         sheet['A4'].alignment = Alignment(horizontal = 'center', vertical='center')
-        sheet['A4'] ="State :"+ gkdata["statename"] + "            "+"Peroid :"+ gkdata["startdate"] + "to" +gkdata["enddate"]
+        sheet['A4'] ="State :"+ gkdata["statename"] + "                  "+"Peroid :"+ gkdata["startdate"] + "to" +gkdata["enddate"]
+
+        sheet.merge_cells('A6:A7')
+        sheet['A6'].font = Font(name='Liberation Serif',size='12',bold=True)
+        sheet['A6'].alignment = Alignment(horizontal = 'center', vertical='center')
+        sheet['A6'] = "Type of Tax"
+
+        sheet.merge_cells(start_row=6, start_column=2, end_row=7, end_column=3)
+        sheet['B6'].font = Font(name='Liberation Serif',size='12',bold=True)
+        sheet['B6'].alignment = Alignment(horizontal = 'center', vertical='center')
+        sheet['B6'] = "Tax Amount"
+  
+        sheet.merge_cells('D6:E6')
+        sheet['D6'].font = Font(name='Liberation Serif',size='12',bold=True)
+        sheet['D6'].alignment = Alignment(horizontal = 'center', vertical='center')
+        sheet['D6'] = "Net Tax Amount"
+
+        sheet['D7'] = "Payable"
+        sheet['E7'] = "Carried Forward "
+
+        sheet.column_dimensions["A"].width = 30
+        sheet.column_dimensions["B"].width = 15
+        sheet.column_dimensions["C"].width = 15
+        sheet.column_dimensions["D"].width = 15
+        sheet.column_dimensions["E"].width = 15
+
+        c = 1
+        r = 7
         
+        if len(result.json()["gkresult"]["sgstin"]) > 0:
+            sheet.cell(row=r,column=c,value= "SGST")
+            sheet.cell(row=r+1,column=c+1,value= "\tInput Tax")
+            for sgstinacc in data["sgstin"]:
+                n = 1
+                
+                
+                
+
+        
+        
+        
+
+        
+
         
         
         gstsmwb.save('report.xlsx')
