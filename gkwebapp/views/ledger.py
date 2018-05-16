66b304f45312b4a2d5fd1a3f736513e35eb7eab3
reshma1 <bhatawadekar1reshma@gmail.com>
2018-05-16 11:39:25 +0530

    unwanted code is removed from ledger.py file.

--- gkwebapp/views/ledger.py
+++ gkwebapp/views/ledger.py
@@ -344,41 +344,35 @@ def showledgerreport(request):
             if projectcode=="":
                 if "orderflag" in request.params:
                     result = requests.get("http://127.0.0.1:6543/report?type=crdrledger&accountcode=%d&calculatefrom=%s&calculateto=%s&financialstart=%s&projectcode=&side=%s&orderflag=%d"%(accountcode,calculatefrom,calculateto,financialstart,request.params["side"],int(request.params["orderflag"])), headers=header)
-                    ledgerrefresh["side"]=request.params["side"]
-                    return render_to_response("gkwebapp:templates/ledgerreport.jinja2",{"records":result.json()["gkresult"],"narrationflag":narrationflag,"userrole":result.json()["userrole"],"ledgerrefresh":ledgerrefresh,"ledgerheader":result.json()["ledgerheader"],"orderflag":"1" },request=request)
+                    orderflag = "1"
                 else:
                     result = requests.get("http://127.0.0.1:6543/report?type=crdrledger&accountcode=%d&calculatefrom=%s&calculateto=%s&financialstart=%s&projectcode=&side=%s"%(accountcode,calculatefrom,calculateto,financialstart,request.params["side"]), headers=header)
-                    ledgerrefresh["side"]=request.params["side"]
-                    return render_to_response("gkwebapp:templates/ledgerreport.jinja2",{"records":result.json()["gkresult"],"narrationflag":narrationflag,"userrole":result.json()["userrole"],"ledgerrefresh":ledgerrefresh,"ledgerheader":result.json()["ledgerheader"],"orderflag":"4" },request=request)
+                    orderflag = "4"
             else:
                 if "orderflag" in request.params:
                     result = requests.get("http://127.0.0.1:6543/report?type=crdrledger&accountcode=%d&calculatefrom=%s&calculateto=%s&financialstart=%s&projectcode=%d&side=%s&orderflag=%d"%(accountcode,calculatefrom,calculateto,financialstart,int(projectcode),request.params["side"],int(request.params["orderflag"])), headers=header)
-                    ledgerrefresh["projectname"] = result.json()["ledgerheader"]["projectname"]
-                    ledgerrefresh["side"]=request.params["side"]
-                    return render_to_response("gkwebapp:templates/ledgerreport.jinja2",{"records":result.json()["gkresult"],"narrationflag":narrationflag,"userrole":result.json()["userrole"],"ledgerrefresh":ledgerrefresh,"ledgerheader":result.json()["ledgerheader"],"orderflag":"1" },request=request)
+                    orderflag = "1"
                 else:
                     result = requests.get("http://127.0.0.1:6543/report?type=crdrledger&accountcode=%d&calculatefrom=%s&calculateto=%s&financialstart=%s&projectcode=%d&side=%s"%(accountcode,calculatefrom,calculateto,financialstart,int(projectcode),request.params["side"]), headers=header)
-                    ledgerrefresh["projectname"] = result.json()["ledgerheader"]["projectname"]
-                    ledgerrefresh["side"]=request.params["side"]
-                    return render_to_response("gkwebapp:templates/ledgerreport.jinja2",{"records":result.json()["gkresult"],"narrationflag":narrationflag,"userrole":result.json()["userrole"],"ledgerrefresh":ledgerrefresh,"ledgerheader":result.json()["ledgerheader"],"orderflag":"4" },request=request)
-            
+                    orderflag = "4"
+                ledgerrefresh["projectname"] = result.json()["ledgerheader"]["projectname"]
+            ledgerrefresh["side"]=request.params["side"]            
         else:
             if projectcode=="":
                 if "orderflag" in request.params:
                     result = requests.get("http://127.0.0.1:6543/report?type=ledger&accountcode=%d&calculatefrom=%s&calculateto=%s&financialstart=%s&orderflag=%d&projectcode="%(accountcode,calculatefrom,calculateto,financialstart,int(request.params["orderflag"])), headers=header)
-                    return render_to_response("gkwebapp:templates/ledgerreport.jinja2",{"records":result.json()["gkresult"],"narrationflag":narrationflag,"userrole":result.json()["userrole"],"ledgerrefresh":ledgerrefresh,"ledgerheader":result.json()["ledgerheader"],"orderflag":"1" },request=request)
+                    orderflag = "1"
                 else:
                     result = requests.get("http://127.0.0.1:6543/report?type=ledger&accountcode=%d&calculatefrom=%s&calculateto=%s&financialstart=%s&projectcode="%(accountcode,calculatefrom,calculateto,financialstart), headers=header)
-                    return render_to_response("gkwebapp:templates/ledgerreport.jinja2",{"records":result.json()["gkresult"],"narrationflag":narrationflag,"userrole":result.json()["userrole"],"ledgerrefresh":ledgerrefresh,"ledgerheader":result.json()["ledgerheader"],"orderflag":"4" },request=request)
+                    orderflag = "4"
             else:
                 if "orderflag" in request.params:
                     result = requests.get("http://127.0.0.1:6543/report?type=ledger&accountcode=%d&calculatefrom=%s&calculateto=%s&financialstart=%s&projectcode=%d&orderflag=%d"%(accountcode,calculatefrom,calculateto,financialstart,int(projectcode),int(request.params["orderflag"])), headers=header)
-                    ledgerrefresh["projectname"] = result.json()["ledgerheader"]["projectname"]
-                    return render_to_response("gkwebapp:templates/ledgerreport.jinja2",{"records":result.json()["gkresult"],"narrationflag":narrationflag,"userrole":result.json()["userrole"],"ledgerrefresh":ledgerrefresh,"ledgerheader":result.json()["ledgerheader"],"orderflag":"1" },request=request)
+                    orderflag = "1"
                 else:
                    result = requests.get("http://127.0.0.1:6543/report?type=ledger&accountcode=%d&calculatefrom=%s&calculateto=%s&financialstart=%s&projectcode=%d"%(accountcode,calculatefrom,calculateto,financialstart,int(projectcode)), headers=header)
-                   ledgerrefresh["projectname"] = result.json()["ledgerheader"]["projectname"]
-                   return render_to_response("gkwebapp:templates/ledgerreport.jinja2",{"records":result.json()["gkresult"],"narrationflag":narrationflag,"userrole":result.json()["userrole"],"ledgerrefresh":ledgerrefresh,"ledgerheader":result.json()["ledgerheader"],"orderflag":"4" },request=request)
+                   orderflag = "4"
+                ledgerrefresh["projectname"] = result.json()["ledgerheader"]["projectname"]
         return render_to_response("gkwebapp:templates/ledgerreport.jinja2",{"records":result.json()["gkresult"],"narrationflag":narrationflag,"userrole":result.json()["userrole"],"ledgerrefresh":ledgerrefresh,"ledgerheader":result.json()["ledgerheader"],"orderflag":orderflag },request=request)
 
 
