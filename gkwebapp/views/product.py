f8a7866c2fa7504b070a9e34aeb83e7a929710a4
p_priyanka <priyankaprabhavale95@gmail.com>
2018-05-31 16:43:19 +0530

    Converted format to float with precision of 2 decimal in product report while exporting into the spreadsheet format.

--- gkwebapp/views/product.py
+++ gkwebapp/views/product.py
@@ -781,13 +781,18 @@ def stockreportspreadsheet(request):
                      sheet['F'+str(row)] = stock["rnno"]
                      sheet['F'+str(row)].font = Font(name='Liberation Serif', size='12', bold=False)
                      sheet['F'+str(row)].alignment = Alignment(horizontal='right')
-                     sheet['G'+str(row)]=float("%.2f"%float(stock["inwardqty"]))
-                     sheet['G'+str(row)].number_format = '0.00'
+                     sheet['G'+str(row)] = stock["inwardqty"]
                      sheet['G'+str(row)].font = Font(name='Liberation Serif', size='12', bold=False)
                      sheet['G'+str(row)].alignment = Alignment(horizontal='right')
+                     if stock["inwardqty"]!= "":
+                         sheet['G'+str(row)]=float("%.2f"%float(stock["inwardqty"]))
+                         sheet['G'+str(row)].number_format = '0.00'
                      sheet['H'+str(row)] = stock["outwardqty"]
                      sheet['H'+str(row)].font = Font(name='Liberation Serif', size='12', bold=False)
                      sheet['H'+str(row)].alignment = Alignment(horizontal='right')
+                     if stock["outwardqty"]!= "":
+                         sheet['H'+str(row)]=float("%.2f"%float(stock["outwardqty"]))
+                         sheet['H'+str(row)].number_format = '0.00'
                      sheet['I'+str(row)]=float("%.2f"%float(stock["balance"]))
                      sheet['I'+str(row)].number_format = '0.00'
                      sheet['I'+str(row)].font = Font(name='Liberation Serif', size='12', bold=False)
