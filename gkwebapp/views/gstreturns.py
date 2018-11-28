a8cf7de5b67e5f218f73785150171f48846d479e
Prajkta_Patkar <prajkta.disroot.org>
2018-11-28 13:15:15 +0530

    when hsn data won't render (condition solved by checking len)

--- gkwebapp/views/gstreturns.py
+++ gkwebapp/views/gstreturns.py
@@ -51,9 +51,9 @@ def gstr1(request):
                           headers=header,
                           params=params)
     result1 = results.json()
-    
-    l = len(result1["gkdata"]["hsn1"]) -1
-    del result1["gkdata"]["hsn1"][l]
+    if len(result1["gkdata"]["hsn1"]) > 0:
+        l = len(result1["gkdata"]["hsn1"]) -1
+        del result1["gkdata"]["hsn1"][l]
     result1["gkdata"]["report_type"] = "r1"
     result1["gkdata"]["startdate"] = params["start"]
     result1["gkdata"]["enddate"] = params["end"]
