390a5a665e5384448a4d1ad625a87967c2c2e6ce
rohan <rohankhairnar@gmail.com>
2018-12-18 18:39:50 +0530

    login done

--- gkwebapp/views/startup.py
+++ gkwebapp/views/startup.py
@@ -83,7 +83,8 @@ def login(request):
     branch = []
     for row in result.json()["gkdata"]:
         branch.append({"bid":int(row["bid"]), "bname":row["bname"]})
-    return {"code":request.params["orgcode"], "flag": request.params["flag"], "branches":branch}
+    blen = len(branch)
+    return {"code":request.params["orgcode"], "flag": request.params["flag"], "branches":branch, "len":blen}
 
 
 @view_config(route_name="createorg", renderer="gkwebapp:templates/createorg.jinja2")
@@ -133,13 +134,16 @@ def orglogin(request):
 
 @view_config(route_name="userlogin",renderer="json")
 def selectorglogin(request):
-    gkdata = {"username":request.params["username"], "userpassword":request.params["userpassword"],"orgcode":request.params["orgcode"]}
+    if "goid" in request.params:
+        gkdata = {"username":request.params["username"], "userpassword":request.params["userpassword"],"orgcode":request.params["orgcode"],"goid":request.params["goid"]}
+    else:
+        gkdata = {"username":request.params["username"], "userpassword":request.params["userpassword"],"orgcode":request.params["orgcode"]}
+    
     result = requests.post("http://127.0.0.1:6543/login", data =json.dumps(gkdata))
     if result.json()["gkstatus"]==0:
         header = result.json()["token"]
         result1 = requests.get("http://127.0.0.1:6543/organisation",headers={"gktoken":header})
         return  {"gktoken":result.json()["token"], "gkstatus":result.json()["gkstatus"],"invflag":result1.json()["gkdata"]["invflag"],"invsflag":result1.json()["gkdata"]["invsflag"],"billflag":result1.json()["gkdata"]["billflag"],"avnoflag":result1.json()["gkdata"]["avnoflag"],"maflag":result1.json()["gkdata"]["maflag"],"avflag":result1.json()["gkdata"]["avflag"],"modeflag":result1.json()["gkdata"]["modeflag"]}
-
     else:
         return  {"gkstatus":result.json()["gkstatus"]}
 
