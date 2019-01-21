00ebcc239da3865f42c1bd9b47ed61999d18bf86
abhi1tb <abhijithb21@disroot.org>
2019-01-21 19:01:19 +0530

    All balance sheet exports corected

--- gkwebapp/views/balancesheet.py
+++ gkwebapp/views/balancesheet.py
@@ -214,13 +214,12 @@ def printconvbalsheetreport(request):
                             sheet['H'+str(row)].alignment = Alignment(horizontal = 'right')
                             sheet['H'+str(row)].font = Font(name='Liberation Serif',size='12',bold=True)
                 row += 1
-        conventionalwb.save('report.xlsx')
-        xlsxfile = open("report.xlsx","r")
-        reportxslx = xlsxfile.read()
-        headerList = {'Content-Type':'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ,'Content-Length': len(reportxslx),'Content-Disposition': 'attachment; filename=report.xlsx', 'Set-Cookie':'fileDownload=true; path=/'}
-        xlsxfile.close()
-        os.remove("report.xlsx")
-        return Response(reportxslx, headerlist=headerList.items())
+        output = cStringIO.StringIO()
+        conventionalwb.save(output)
+        contents = output.getvalue()
+        output.close()
+        headerList = {'Content-Type':'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ,'Content-Length': len(contents),'Content-Disposition': 'attachment; filename=report.xlsx', 'Set-Cookie':'fileDownload=true; path=/'}
+        return Response(contents, headerlist=headerList.items())
     except:
         print "File not Found"
         {"gkstatus":3}
@@ -389,13 +388,12 @@ def printconsbalsheetreport(request):
                         sheet['H'+str(row)].alignment = Alignment(horizontal = 'right')
                         sheet['H'+str(row)].font = Font(name='Liberation Serif',size='12',bold=True)
                 row += 1
-        consolidationwb.save('report.xlsx')
-        xlsxfile = open("report.xlsx","r")
-        reportxslx = xlsxfile.read()
-        headerList = {'Content-Type':'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ,'Content-Length': len(reportxslx),'Content-Disposition': 'attachment; filename=report.xlsx', 'Set-Cookie':'fileDownload=true; path=/'}
-        xlsxfile.close()
-        os.remove("report.xlsx")
-        return Response(reportxslx, headerlist=headerList.items())        
+        output = cStringIO.StringIO()
+        consolidationwb.save(output)
+        contents = output.getvalue()
+        output.close()
+        headerList = {'Content-Type':'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' ,'Content-Length': len(contents),'Content-Disposition': 'attachment; filename=report.xlsx', 'Set-Cookie':'fileDownload=true; path=/'}
+        return Response(contents, headerlist=headerList.items())
     except:
         print "File not Found"
         {"gkstatus":3}
