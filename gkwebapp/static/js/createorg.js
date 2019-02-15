2e531118a9dc7311c7e699c03408c88f6d6e861e
rohan <rohankhairnar@gmail.com>
2019-02-15 13:11:00 +0530

    branch id issue after creating new org

--- gkwebapp/static/js/createorg.js
+++ gkwebapp/static/js/createorg.js
@@ -582,6 +582,8 @@ $(document).ready(function(){
         sessionStorage.setItem('invsflag', invsflag );
         sessionStorage.setItem('billflag', billflag );
         sessionStorage.setItem('modeflag', modeflag);
+        sessionStorage.goid = '';
+        sessionStorage.goname = '';
         $.ajax({
           url: '/oexists',
           type: 'POST',
