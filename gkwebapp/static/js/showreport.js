201f0c26e614b33edb7d293c0267d067b21de743
rupali777 <rupalibdgujar1234@gmail.com>
2019-04-08 17:28:01 +0530

    key navigation work

---
+++ gkwebapp/static/js/showreport.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#report_li').click(function(event) {
+  console.log("dfdfdf");
+        $("#showviewledger").focus();
+      $('.focuscls:first').closest('div').addClass('liclass');
+      });
+
+});
