58e040776acfe08f97ad36bd3d89bd938d22beea
rupali777 <rupalibdgujar1234@gmail.com>
2019-04-09 17:42:07 +0530

    key navigation for all divs

--- gkwebapp/static/js/showreport.js
+++ gkwebapp/static/js/showreport.js
@@ -1,17 +1,9 @@
 $(document).ready(function(){
 
-  var currentindex;
+var currentindex;
 var rowcount;
 var currentrow;
 
-$(document).off('keydown' ,'#accountingrepdiv').on('keydown' ,'#accountingrepdiv',function(event) {
-  if (event.altKey && event.shiftKey && event.keyCode == 40) {
-    event.preventDefault();
-    $('.accountingfocus').closest('div').removeClass('keyclass');
-    $('#inventoryfocus:first').focus();
-    $('#inventoryfocus:first').addClass('keyclass');
-  }
-});
   $(document).off('keydown' ,'.accountingfocus').on('keydown' ,'.accountingfocus',function(event) {
     event.preventDefault();
   currentrow = $(this).closest("div");
@@ -37,15 +29,15 @@ $(document).off('keydown' ,'#accountingrepdiv').on('keydown' ,'#accountingrepdiv
     }
 
   });
-
-  // $(document).off('keydown' ,'#inventoryrepdiv').on('keydown' ,'#inventoryrepdiv',function(event) {
-  //   if (event.altKey && event.shiftKey && event.keyCode == 40) {
-  //     event.preventDefault();
-  //     $('.accountingfocus').closest('div').removeClass('keyclass');
-  //     $('.inventoryfocus:first').focus();
-  //     $('.inventoryfocus:first').addClass('keyclass');
-  //   }
-  // });
+  $(document).off('keydown' ,'#accountingrepdiv').on('keydown' ,'#accountingrepdiv',function(event) {
+    if (event.altKey && event.shiftKey && event.keyCode == 40) {
+      event.preventDefault();
+      $('.accountingfocus').closest('div').removeClass('keyclass');
+      $('.inventoryfocus:first').focus();
+      $('.inventoryfocus:first').closest('div').addClass('keyclass');
+    }
+    
+  });
 
     $(document).off('keydown' ,'.inventoryfocus').on('keydown' ,'.inventoryfocus',function(event) {
       event.preventDefault();
@@ -72,4 +64,95 @@ $(document).off('keydown' ,'#accountingrepdiv').on('keydown' ,'#accountingrepdiv
       }
   
     });
+
+  $(document).off('keydown' ,'#inventoryrepdiv').on('keydown' ,'#inventoryrepdiv',function(event) {
+    if (event.altKey && event.shiftKey && event.keyCode == 40) {
+      event.preventDefault();
+      $('.inventoryfocus').closest('div').removeClass('keyclass');
+      $('.documentfocus:first').focus();
+      $('.documentfocus:first').closest('div').addClass('keyclass');
+    }
+    if (event.altKey && event.shiftKey && event.keyCode == 38) {
+      event.preventDefault();
+      $('.inventoryfocus').closest('div').removeClass('keyclass');
+      $('.accountingfocus:first').focus();
+      $('.accountingfocus:first').closest('div').addClass('keyclass');
+    }
+  });
+
+      $(document).off('keydown' ,'.documentfocus').on('keydown' ,'.documentfocus',function(event) {
+        event.preventDefault();
+      currentrow = $(this).closest("div");
+      currentindex = $(currentrow).index();
+      rowcount =$(".documentfocus").length-1;
+    
+        if (event.which == 13){
+          $(this).closest("div").click();
+        }
+        if (event.which==40 && currentindex != rowcount)
+        {
+          event.preventDefault();
+        $(currentrow).removeClass("keyclass");
+        $(currentrow).next().find("a").focus(); 
+        $(currentrow).next().addClass("keyclass");
+        }
+        else if (event.which==38  && currentindex != 0)
+        {
+          event.preventDefault();
+          $(currentrow).removeClass("keyclass");
+          $(currentrow).prev().find("a").focus(); 
+          $(currentrow).prev().addClass("keyclass");
+        }
+    
+      });
+
+      $(document).off('keydown' ,'#documentsrepdiv').on('keydown' ,'#documentsrepdiv',function(event) {
+        if (event.altKey && event.shiftKey && event.keyCode == 40) {
+          event.preventDefault();
+          $('.documentfocus').closest('div').removeClass('keyclass');
+          $('.administrationfocus:first').focus();
+          $('.administrationfocus:first').closest('div').addClass('keyclass');
+        }
+        if (event.altKey && event.shiftKey && event.keyCode == 38) {
+          event.preventDefault();
+          $('.documentfocus').closest('div').removeClass('keyclass');
+          $('.inventoryfocus:first').focus();
+          $('.inventoryfocus:first').closest('div').addClass('keyclass');
+        }
+      });
+    $(document).off('keydown' ,'.administrationfocus').on('keydown' ,'.administrationfocus',function(event) {
+        event.preventDefault();
+      currentrow = $(this).closest("div");
+      currentindex = $(currentrow).index();
+      rowcount =$(".administrationfocus").length-1;
+    
+        if (event.which == 13){
+          $(this).closest("div").click();
+        }
+        if (event.which==40 && currentindex != rowcount)
+        {
+          event.preventDefault();
+        $(currentrow).removeClass("keyclass");
+        $(currentrow).next().find("a").focus(); 
+        $(currentrow).next().addClass("keyclass");
+        }
+        else if (event.which==38  && currentindex != 0)
+        {
+          event.preventDefault();
+          $(currentrow).removeClass("keyclass");
+          $(currentrow).prev().find("a").focus(); 
+          $(currentrow).prev().addClass("keyclass");
+        }
+    
+      });
+
+      $(document).off('keydown' ,'#administrationrepdiv').on('keydown' ,'#administrationrepdiv',function(event) {
+        if (event.altKey && event.shiftKey && event.keyCode == 38) {
+          event.preventDefault();
+          $('.administrationfocus').closest('div').removeClass('keyclass');
+          $('.documentfocus:first').focus();
+          $('.documentfocus:first').closest('div').addClass('keyclass');
+        }
+      });
+  
 });
