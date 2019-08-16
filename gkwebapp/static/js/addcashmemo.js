8ea6edd3639786c044f4d4bddb28c8c1704cff2c
abhijith <abhijith@dff.org.in>
2019-08-16 10:53:42 +0530

    Adjustments for discount in Cash Memo print

--- gkwebapp/static/js/addcashmemo.js
+++ gkwebapp/static/js/addcashmemo.js
@@ -158,6 +158,9 @@ $(document).ready(function() {
 	var rowqty = parseFloat($('#invoice_product_table_gst tbody tr:eq(' + curindex + ') td:eq(2) input').val()).toFixed(2);
 	var rowprice = parseFloat($('#invoice_product_table_gst tbody tr:eq(' + curindex + ') td:eq(4) input').val()).toFixed(2);
 	var rowdiscount = parseFloat($('#invoice_product_table_gst tbody tr:eq(' + curindex + ') td:eq(5) input').val()).toFixed(2);
+	if ($("#discountpercent").val() == 16){
+            rowdiscount = (rowqty * rowprice * rowdiscount)/100;
+        }
 	var taxamount = 0.00;
 	var rowtaxableamount=(rowqty * rowprice) - rowdiscount; //Taxable amount for each row is calculated.
 	if ($('#invoice_product_table_gst tbody tr:eq(' + curindex + ') td:eq(2) input').is(":disabled") && $('#invoice_product_table_gst tbody tr:eq(' + curindex + ') td:eq(3) input').is(":disabled")) {
