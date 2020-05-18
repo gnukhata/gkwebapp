80cf86a2efa8bc6a467d65c9fd301e249ee7d5a8
Prajkta_Patkar <prajkta@disroot.org>
2020-05-18 15:00:35 +0530

    removed print st

--- gkwebapp/views/projectstatement.py
+++ gkwebapp/views/projectstatement.py
@@ -145,7 +145,6 @@ def showprojectstatementreport(request):
     projectname = request.params["projectname"]
     header={"gktoken":request.headers["gktoken"]}
     result = requests.get("http://127.0.0.1:6543/report?type=projectstatement&calculateto=%s&financialstart=%s&projectcode=%d"%(calculateto,financialstart,projectcode), headers=header)
-    print (result.json()["gkresult"])
     return render_to_response("gkwebapp:templates/projectstatementreport.jinja2",{"records":result.json()["gkresult"],"projectcode":projectcode,"projectname":projectname,"from":datetime.strftime(datetime.strptime(str(financialstart),"%Y-%m-%d").date(),'%d-%m-%Y'),"to":datetime.strftime(datetime.strptime(str(calculateto),"%Y-%m-%d").date(),'%d-%m-%Y')},request=request)
 
 @view_config(route_name="printprojectstatement")
