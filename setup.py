46ac3f9926de2951f757c5184528439bc0053752
Sai Karthik <kskarthik@disroot.org>
2022-02-03 16:53:51 +0530

    upddate pyramid to v2.0

--- setup.py
+++ setup.py
@@ -23,47 +23,48 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.txt')) as f:
+with open(os.path.join(here, "README.txt")) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
+with open(os.path.join(here, "CHANGES.txt")) as f:
     CHANGES = f.read()
 
 requires = [
-    'pyramid == 1.10.5',
-    'openpyxl == 2.5.0',
-    'pyramid_jinja2 == 2.8',
-    'WebOb == 1.8.6',
-    'gunicorn == 20.1.0',
+    "pyramid == 2.0",  # 1.10.5'
+    "openpyxl == 2.5.0",
+    "pyramid_jinja2 == 2.8",
+    "WebOb == 1.8.6",
+    "gunicorn == 20.1.0",
     #'waitress == 1.4.4',
-    'wsgicors == 0.7.0',
-    'requests == 2.25.0',
-    'Pillow == 8.0.1',
-    'supervisor == 4.2.1',
-    'Babel == 2.9.0',
+    "wsgicors == 0.7.0",
+    "requests == 2.25.0",
+    "Pillow == 8.0.1",
+    "supervisor == 4.2.1",
+    "Babel == 2.9.0",
 ]
 
-setup(name='gkwebapp',
-      version='0.0',
-      description='gkwebapp',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-          "Programming Language :: Python",
-          "Framework :: Pyramid",
-          "Topic :: Internet :: WWW/HTTP",
-          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-      ],
-      author='',
-      author_email='',
-      url='',
-      keywords='web pyramid pylons',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=requires,
-      tests_require=requires,
-      test_suite="gkwebapp",
-      entry_points="""\
+setup(
+    name="gkwebapp",
+    version="0.0",
+    description="gkwebapp",
+    long_description=README + "\n\n" + CHANGES,
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+    ],
+    author="",
+    author_email="",
+    url="",
+    keywords="web pyramid pylons",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    tests_require=requires,
+    test_suite="gkwebapp",
+    entry_points="""\
       [paste.app_factory]
       main = gkwebapp:main
       """,
-      )
+)
